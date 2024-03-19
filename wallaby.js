1c55aade72a01e451015607664173690bc6180e0
Oleg Pimenov <pimenov.o@gmail.com>
2024-03-19 16:19:34 +0300

    chore: added wallaby

---
+++ wallaby.js
@@ -0,0 +1,14 @@
+
+module.exports = () => ({
+    autoDetect: true,
+    files: [
+        'packages/**/*.ts',
+        "!packages/**/*.spec.ts",
+    ],
+    tests: [
+        "packages/**/*.spec.ts",
+        "!packages/docs/**/*.*",
+        "!packages/components-dev/**/*.*",
+        "!packages/docs-examples/**/*.*"
+    ]
+});
