e44b9819fe0245bbee81939d905e124a61e292dd
Oleg Pimenov <pimenov.o@gmail.com>
2024-04-28 14:30:31 +0300

    chore: upd linters

--- tools/gulp/gulpfile.ts
+++ tools/gulp/gulpfile.ts
@@ -1,6 +1,4 @@
 /* tslint:disable:no-import-side-effect ordered-imports */
 // THIS ORDER OF IMPORTS AND CALLS IS IMPORTANT
 
-import './tasks/lint';
 import './tasks/styles';
-import './tasks/release';
