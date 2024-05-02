3eee7bea2a9d2be2fc6232acd5f9702ec3df15bc
Oleg Pimenov <pimenov.o@gmail.com>
2024-05-02 14:23:01 +0300

    chore: remove unused code

--- scripts/deploy/deploy-to-preview-channel.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -o pipefail
-
-FIREBASE_DEPLOY_RESPONSE=$(npx firebase hosting:channel:deploy sha-$CI_COMMIT_SHORT_SHA \
-    --only next \
-    --expires 5d \
-    --token=${FIREBASE_DEPLOY_TOKEN} --json)
