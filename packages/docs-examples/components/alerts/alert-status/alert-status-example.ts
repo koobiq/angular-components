5784c6b58a1dd3ed6616c6de8e5b2f0bdc943b17
Nikita Guryev <nikgurv@gmail.com>
2024-05-31 17:27:30 +0300

    feat(docs): examples & layout fix

---
+++ packages/docs-examples/components/alerts/alert-status/alert-status-example.ts
@@ -0,0 +1,31 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { KbqButtonStyles } from '@koobiq/components/button';
+import { KbqComponentColors } from '@koobiq/components/core';
+import { KbqAlertColors, KbqAlertStyles } from '@koobiq/components/alert';
+
+
+/**
+ * @title Alert Status
+ */
+@Component({
+    selector: 'alert-status-example',
+    templateUrl: 'alert-status-example.html',
+    styleUrls: ['alert-status-example.css'],
+    animations: [
+        trigger('hideShowAnimator', [
+            state('true' , style({ opacity: 1, display: '' })),
+            state('false', style({ opacity: 0, display: 'none' })),
+            transition('false => true', animate('.5s')),
+            transition('true => false', animate('.2s'))
+        ])
+    ],
+    encapsulation: ViewEncapsulation.None
+})
+export class AlertStatusExample {
+    colors = KbqComponentColors;
+    style = KbqButtonStyles;
+    alertStyles = KbqAlertStyles;
+    alertColors = KbqAlertColors;
+    text = 'Если нет заголовка, не ставьте точку в конце последнего предложения. Если сообщение содержит основной текст и заголовок — ставьте точку в конце основного текста.';
+}
