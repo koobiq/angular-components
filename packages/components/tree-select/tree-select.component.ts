5374d3594c2cd4ff6f7ce22dcf853b3e5b2980f8
Nikita Guryev <nikgurv@gmail.com>
2024-07-23 11:03:36 +0300

    fix(tree-select): CR fix

--- packages/components/tree-select/tree-select.component.ts
+++ packages/components/tree-select/tree-select.component.ts
@@ -477,7 +477,7 @@ export class KbqTreeSelect
     ) {
         super(elementRef, defaultErrorStateMatcher, parentForm, parentFormGroup, ngControl);
 
-        this.localeService?.changes.subscribe(this.updateLocaleParams);
+        this.localeService?.changes.pipe(takeUntil(this.destroy)).subscribe(this.updateLocaleParams);
 
         if (this.ngControl) {
             // Note: we provide the value accessor through here, instead of
