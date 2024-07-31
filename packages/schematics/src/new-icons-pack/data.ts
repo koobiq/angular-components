443bc14e156ee8bc7c8188a6d88a4b28f5acab51
Nikita Guryev <nikgurv@gmail.com>
2024-07-31 12:39:19 +0300

    feat(icon): updated replacement data

--- packages/schematics/src/new-icons-pack/data.ts
+++ packages/schematics/src/new-icons-pack/data.ts
@@ -1,4 +1,3 @@
-
 export interface ReplaceData {
     replace: string;
     replaceWith: string;
@@ -7,381 +6,347 @@ export interface ReplaceData {
 
 export const newIconsPackData: ReplaceData[] = [
     {
-        'replace': 'angle-down-L_16',
-        'replaceWith': 'angle-down_16'
+        replace: 'angle-down-L_16',
+        replaceWith: 'chevron-down_16'
     },
     {
-        'replace': 'angle-down-M_16',
-        'replaceWith': 'angle-down_16'
+        replace: 'angle-down-L_24',
+        replaceWith: 'chevron-down_24'
     },
     {
-        'replace': 'angle-down-S_16',
-        'replaceWith': 'angle-down_16'
+        replace: 'angle-down-M_16',
+        replaceWith: 'chevron-down_16'
     },
     {
-        'replace': 'angle-left-L_16',
-        'replaceWith': 'angle-left_16'
+        replace: 'angle-down-M_24',
+        replaceWith: 'chevron-down_24'
     },
     {
-        'replace': 'angle-left-M_16',
-        'replaceWith': 'angle-left_16'
+        replace: 'angle-down-S_16',
+        replaceWith: 'chevron-down-s_16'
     },
     {
-        'replace': 'angle-left-S_16',
-        'replaceWith': 'angle-left_16'
+        replace: 'angle-left-L_16',
+        replaceWith: 'chevron-left_16'
     },
     {
-        'replace': 'angle-right-L_16',
-        'replaceWith': 'angle-right_16'
+        replace: 'angle-left-L_24',
+        replaceWith: 'chevron-left_24'
     },
     {
-        'replace': 'angle-right-M_16',
-        'replaceWith': 'angle-right_16'
+        replace: 'angle-left-M_16',
+        replaceWith: 'chevron-left_16'
     },
     {
-        'replace': 'angle-right-S_16',
-        'replaceWith': 'angle-right_16'
+        replace: 'angle-left-M_24',
+        replaceWith: 'chevron-left_24'
     },
     {
-        'replace': 'angle-up-L_16',
-        'replaceWith': 'angle-up_16'
+        replace: 'angle-left-S_16',
+        replaceWith: 'chevron-left-s_16'
     },
     {
-        'replace': 'angle-up-M_16',
-        'replaceWith': 'angle-up_16'
+        replace: 'angle-right-L_16',
+        replaceWith: 'chevron-right_16'
     },
     {
-        'replace': 'angle-up-S_16',
-        'replaceWith': 'angle-up_16'
+        replace: 'angle-right-L_24',
+        replaceWith: 'chevron-right_24'
     },
     {
-        'replace': 'arrow-thin-up_16',
-        'replaceWith': 'text symbol ↑'
+        replace: 'angle-right-M_16',
+        replaceWith: 'chevron-right_16'
     },
     {
-        'replace': 'arrow-thin-down_16',
-        'replaceWith': 'text symbol ↓'
+        replace: 'angle-right-M_24',
+        replaceWith: 'chevron-right_24'
     },
     {
-        'replace': 'arrow-thin-left_16',
-        'replaceWith': 'text symbol ←'
+        replace: 'angle-right-M_32',
+        replaceWith: 'chevron-right-m_32'
     },
     {
-        'replace': 'arrow-thin-right_16',
-        'replaceWith': 'text symbol →'
+        replace: 'angle-right-S_16',
+        replaceWith: 'chevron-right-s_16'
     },
     {
-        'replace': 'arrow-back_16',
-        'replaceWith': 'arrow-left_16'
+        replace: 'angle-up-L_16',
+        replaceWith: 'chevron-up_16'
     },
     {
-        'replace': 'arrow-forward_16',
-        'replaceWith': 'arrow-right_16'
+        replace: 'angle-up-L_24',
+        replaceWith: 'chevron-up_24'
     },
     {
-        'replace': 'vulner-level-high_16',
-        'replaceWith': 'arrow-up_16'
+        replace: 'angle-up-M_16',
+        replaceWith: 'chevron-up_16'
     },
     {
-        'replace': 'vulner-level-low_16',
-        'replaceWith': 'arrow-down_16'
+        replace: 'angle-up-M_24',
+        replaceWith: 'chevron-up_24'
     },
     {
-        'replace': 'arrow-small-down_16',
-        'replaceWith': 'arrow-down_16'
+        replace: 'angle-up-S_16',
+        replaceWith: 'chevron-up-s_16'
     },
     {
-        'replace': 'arrow-small-left_16',
-        'replaceWith': 'arrow-left_16'
+        replace: 'bell_16',
+        replaceWith: 'bell_16'
     },
     {
-        'replace': 'arrow-small-right_16',
-        'replaceWith': 'arrow-right_16'
+        replace: 'bell_32',
+        replaceWith: 'bell_32'
     },
     {
-        'replace': 'arrow-small-up_16',
-        'replaceWith': 'arrow-up_16'
+        replace: 'bell-o_16',
+        replaceWith: 'bell_16'
     },
     {
-        'replace': 'asset-host-virtual_16',
-        'replaceWith': 'question-circle_16'
+        replace: 'bell-o_24',
+        replaceWith: 'bell_24'
     },
     {
-        'replace': 'asset-network-dummy_16',
-        'replaceWith': 'question-circle_16'
+        replace: 'bell-off-o_16',
+        replaceWith: 'bell-slash_16'
     },
     {
-        'replace': 'asset-network_16',
-        'replaceWith': 'asset-cloud_16'
+        replace: 'bento_16',
+        replaceWith: 'bento-menu_16'
     },
     {
-        'replace': 'asset-node_16',
-        'replaceWith': 'question-circle_16'
+        replace: 'bento_24',
+        replaceWith: 'bento-menu_24'
     },
     {
-        'replace': 'asset-other_16',
-        'replaceWith': 'question-circle_16'
+        replace: 'bento_32',
+        replaceWith: 'bento-menu_32'
     },
     {
-        'replace': 'asset-website_16',
-        'replaceWith': 'globe_16'
+        replace: 'calendar_16',
+        replaceWith: 'calendar-o_16'
     },
     {
-        'replace': 'bell-o_16',
-        'replaceWith': 'bell_16'
+        replace: 'chart-bar_24',
+        replaceWith: 'chart-bar_24'
     },
     {
-        'replace': 'bell-off-o_16',
-        'replaceWith': 'bell-slash_16'
+        replace: 'check_16',
+        replaceWith: 'check_16'
     },
     {
-        'replace': 'event-raw-o_16',
-        'replaceWith': 'event-raw_16'
+        replace: 'circle-8_16',
+        replaceWith: 'circle-6_16'
     },
     {
-        'replace': 'circle-outline_16',
-        'replaceWith': 'circle-empty_16'
+        replace: 'clock_16',
+        replaceWith: 'clock_16'
     },
     {
-        'replace': 'circle-successfull-o_16',
-        'replaceWith': 'success_16'
+        replace: 'close-circle_16',
+        replaceWith: 'xmark-circle_16'
     },
     {
-        'replace': 'circle-successfull_16',
-        'replaceWith': 'success_16'
+        replace: 'close-L_16',
+        replaceWith: 'xmark_16'
     },
     {
-        'replace': 'close-circle-o_16',
-        'replaceWith': 'close-circle_16'
+        replace: 'close-L_32',
+        replaceWith: 'xmark_32'
     },
     {
-        'replace': 'close-circle-small_16',
-        'replaceWith': 'close-circle_16'
+        replace: 'close-M_16',
+        replaceWith: 'xmark_16'
     },
     {
-        'replace': 'criticality-medium_16',
-        'replaceWith': 'importance-3_16'
+        replace: 'close-S_16',
+        replaceWith: 'xmark-s_16'
     },
     {
-        'replace': 'event-normalized-o_16',
-        'replaceWith': 'event-normalized_16'
+        replace: 'cloud-404_256',
+        replaceWith: 'cloud-404_256'
     },
     {
-        'replace': 'filter-active-o_16',
-        'replaceWith': 'filter-active_16'
+        replace: 'color-palette_16',
+        replaceWith: 'palette_16'
     },
     {
-        'replace': 'filter-o_16',
-        'replaceWith': 'filter_16'
+        replace: 'copy_16',
+        replaceWith: 'square-multiple-o_16'
     },
     {
-        'replace': 'importance-2_16',
-        'replaceWith': 'arrow-up_16'
+        replace: 'copy-o_16',
+        replaceWith: 'file-multiple-o_16'
     },
     {
-        'replace': 'importance-5_16',
-        'replaceWith': 'arrow-down_16'
+        replace: 'download_16',
+        replaceWith: 'arrow-down-to-line_16'
     },
     {
-        'replace': 'info-circle_16',
-        'replaceWith': 'info_16'
+        replace: 'download_32',
+        replaceWith: 'cloud-arrow-down_32'
     },
     {
-        'replace': 'info-o_16',
-        'replaceWith': 'info_16'
+        replace: 'ellipsis_16',
+        replaceWith: 'ellipsis-vertical_16'
     },
     {
-        'replace': 'lock-locked-o_16',
-        'replaceWith': 'lock-locked_16'
+        replace: 'ellipsis_24',
+        replaceWith: 'ellipsis-vertical_24'
     },
     {
-        'replace': 'map_16',
-        'replaceWith': 'chart-map_16'
+        replace: 'error_16',
+        replaceWith: 'exclamation-triangle_16'
     },
     {
-        'replace': 'minus-circle-xs_16',
-        'replaceWith': 'minus-circle-small_16'
+        replace: 'external-link_16',
+        replaceWith: 'arrow-up-right-from-square_16'
     },
     {
-        'replace': 'play-o_16',
-        'replaceWith': 'play_16'
+        replace: 'eye_16',
+        replaceWith: 'eye_16'
     },
     {
-        'replace': 'plug-o_16',
-        'replaceWith': 'plug_16'
+        replace: 'eye-crossed_16',
+        replaceWith: 'eye-slash_16'
     },
     {
-        'replace': 'question-circle-o_16',
-        'replaceWith': 'question-circle_16'
+        replace: 'file-empty_16',
+        replaceWith: 'file_16'
     },
     {
-        'replace': 'report-o_16',
-        'replaceWith': 'file-empty_16'
+        replace: 'folder-closed_16',
+        replaceWith: 'folder_16'
     },
     {
-        'replace': 'report_16',
-        'replaceWith': 'file-empty_16'
+        replace: 'folder-open-o_24',
+        replaceWith: 'folder-open_24'
     },
     {
-        'replace': 'success-small_16',
-        'replaceWith': 'success_16'
+        replace: 'folder-opened_16',
+        replaceWith: 'folder-open_16'
     },
     {
-        'comment': 'change to 16/common/kbq-icons-user_16',
-        'replace': 'asset-account_16',
-        'replaceWith': 'user_16'
+        replace: 'folder-search_16',
+        replaceWith: 'folder-badge-magnifying-glass_16'
     },
     {
-        'comment': 'change to 16/common/kbq-icons-user_16',
-        'replace': 'user-o_16',
-        'replaceWith': 'user_16'
+        replace: 'folder-search-opened_16',
+        replaceWith: 'folder-open-badge-magnifying-glass_16'
     },
     {
-        'replace': 'word-wrap_16',
-        'replaceWith': 'wrap-text_16'
+        replace: 'gear_16',
+        replaceWith: 'gear_16'
     },
     {
-        'replace': 'word-wrap-slash_16',
-        'replaceWith': 'word-wrap-slash_16'
+        replace: 'globe_16',
+        replaceWith: 'globe_16'
     },
     {
-        'replace': 'close-M_16',
-        'replaceWith': 'close_16'
+        replace: 'hamburger_16',
+        replaceWith: 'hamburger-menu_16'
     },
     {
-        'replace': 'close-L_16',
-        'replaceWith': 'close_16'
+        replace: 'hamburger_32',
+        replaceWith: 'hamburger-menu_32'
     },
     {
-        'replace': 'close-S_16',
-        'replaceWith': 'close_16'
+        replace: 'info_16',
+        replaceWith: 'info-circle_16'
     },
     {
-        'replace': 'importance-4_16',
-        'replaceWith': 'minus_16'
+        replace: 'info-o_16',
+        replaceWith: 'info-circle-o_16'
     },
     {
-        'replace': 'importance-1_16',
-        'replaceWith': 'importance-1_16'
+        replace: 'list_24',
+        replaceWith: 'list_24'
     },
     {
-        'replace': 'arrows-left_16',
-        'replaceWith': 'arrows-left_16'
+        replace: 'minus_16',
+        replaceWith: 'minus_16'
     },
     {
-        'replace': 'significance-na_16',
-        'replaceWith': 'significance-low_16'
+        replace: 'new-tab_16',
+        replaceWith: 'window-plus_16'
     },
     {
-        'replace': 'significance-low_16',
-        'replaceWith': 'significance-low_16'
+        replace: 'pause_16',
+        replaceWith: 'pause_16'
     },
     {
-        'replace': 'limit_16',
-        'replaceWith': 'scissors_16'
+        replace: 'play_16',
+        replaceWith: 'play_16'
     },
     {
-        'replace': 'topology-star_16',
-        'replaceWith': 'topology-star_16'
+        replace: 'question-circle_24',
+        replaceWith: 'question-circle_24'
     },
     {
-        'replace': 'topology-lines_16',
-        'replaceWith': 'topology-lines_16'
+        replace: 'search_16',
+        replaceWith: 'magnifying-glass_16'
     },
     {
-        'replace': 'vulner-level-medium_16',
-        'replaceWith': 'stop_16'
+        replace: 'search_24',
+        replaceWith: 'magnifying-glass_24'
     },
     {
-        'replace': 'compliance-level-not-defined_16',
-        'replaceWith': 'compliance-level-low_16'
+        replace: 'stop_16',
+        replaceWith: 'stop_16'
     },
     {
-        'replace': 'optical-discdrive_16',
-        'replaceWith': 'band-aid_16'
+        replace: 'success_16',
+        replaceWith: 'check-circle_16'
     },
     {
-        'replace': 'users-group-o_24',
-        'replaceWith': 'users-group_24'
+        replace: 'theme_16',
+        replaceWith: 'theme-mode_16'
     },
     {
-        'replace': 'report-o_24',
-        'replaceWith': 'file-empty_24'
+        replace: 'triangle-down_16',
+        replaceWith: 'location-chevron-down_16'
     },
     {
-        'replace': 'report_24',
-        'replaceWith': 'file-empty_24'
+        replace: 'triangle-left_16',
+        replaceWith: 'location-chevron-left_16'
     },
     {
-        'replace': 'circle-successfull_24',
-        'replaceWith': 'success_24'
+        replace: 'triangle-right_16',
+        replaceWith: 'location-chevron-right_16'
     },
     {
-        'replace': 'update-o_24',
-        'replaceWith': 'update_24'
+        replace: 'triangle-up_16',
+        replaceWith: 'location-chevron-up_16'
     },
     {
-        'replace': 'info-o_24',
-        'replaceWith': 'info_24'
+        replace: 'upload-to-cloud_16',
+        replaceWith: 'arrow-up-from-line_16'
     },
     {
-        'replace': 'asset-account_24',
-        'replaceWith': 'user_24'
+        replace: 'upload-to-cloud_24',
+        replaceWith: 'arrow-up-from-line_24'
     },
     {
-        'replace': 'email-notification-o_24',
-        'replaceWith': 'envelope-partial_24'
+        replace: 'upload-to-cloud_32',
+        replaceWith: 'arrow-up-from-line_32'
     },
     {
-        'replace': 'email-notification_24',
-        'replaceWith': 'envelope-partial_24'
+        replace: 'upload-to-cloud_64',
+        replaceWith: 'arrow-up-from-line_64'
     },
     {
-        'replace': 'lock-locked-o_24',
-        'replaceWith': 'lock-locked_24'
+        replace: 'user_16',
+        replaceWith: 'user_16'
     },
     {
-        'replace': 'folder-open-o_24',
-        'replaceWith': 'folder-open_24'
+        replace: 'user-o_24',
+        replaceWith: 'user_24'
     },
     {
-        'replace': 'log_24',
-        'replaceWith': 'log-o_24'
+        replace: 'word-wrap_16',
+        replaceWith: 'word-wrap_16'
     },
     {
-        'replace': 'connection-o_24',
-        'replaceWith': 'asset-network-device_24'
-    },
-    {
-        'replace': 'connection_24',
-        'replaceWith': 'asset-network-device_24'
-    },
-    {
-        'replace': 'gear-o_24',
-        'replaceWith': 'gear_24'
-    },
-    {
-        'replace': 'asset-other-2_24',
-        'replaceWith': 'question-circle_24'
-    },
-    {
-        'replace': 'asset-host-virtual_24',
-        'replaceWith': 'question-circle_24'
-    },
-    {
-        'replace': 'plug-o_24',
-        'replaceWith': 'plug_24'
-    },
-    {
-        'replace': 'branch-o_24',
-        'replaceWith': 'branch_24'
-    },
-    {
-        'replace': 'circle-warning_24',
-        'replaceWith': 'attention_24'
-    },
-    {
-        'replace': 'close-circle-o_24',
-        'replaceWith': 'close-circle_24'
+        replace: 'word-wrap-slash_16',
+        replaceWith: 'word-wrap-slash_16'
     }
 ];
