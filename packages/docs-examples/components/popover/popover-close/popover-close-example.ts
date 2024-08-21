44fe581198ba7e6f30c7b238709ea78adfd49917
Aleksandr Knutov <sk8knut@gmail.com>
2024-08-21 16:31:02 +0300

    share component to last close example

--- packages/docs-examples/components/popover/popover-close/popover-close-example.ts
+++ packages/docs-examples/components/popover/popover-close/popover-close-example.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { KbqComponentColors } from '@koobiq/components/core';
 
 /**
  * @title popover-close
@@ -9,4 +10,18 @@ import { Component, ViewEncapsulation } from '@angular/core';
     styleUrls: ['popover-close-example.css'],
     encapsulation: ViewEncapsulation.None
 })
-export class PopoverCloseExample {}
+export class PopoverCloseExample {
+    members: object[] = [
+        { name: 'Alexandr Vsekhvalnov', role: 'Editor' },
+        { name: 'Sergey Voximplant', role: 'Editor' },
+        { name: 'Roman Turov', role: 'Viewer' },
+        { name: 'Viber Curly', role: 'Viewer' },
+        { name: 'Jackie Ckang', role: 'Viewer' },
+        { name: 'Robert Skinner', role: 'Viewer' },
+        { name: 'Woodie Hoodie', role: 'Viewer' },
+        { name: 'Alex Buckmaster', role: 'Viewer' },
+        { name: 'Chris Glasser', role: 'Viewer' },
+        { name: 'Corina McCoy', role: 'Viewer' }
+    ];
+    protected readonly componentColors = KbqComponentColors;
+}
