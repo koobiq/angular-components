a06249210e596480ddf1b25b181be5b0b15b0b8d
lskramarov <l.s.kramarov@gmail.com>
2024-08-26 17:36:04 +0300

    feat(docs): examples for datepicker (#DS-2744)

--- packages/docs-examples/components/datepicker/index.ts
+++ packages/docs-examples/components/datepicker/index.ts
@@ -2,25 +2,37 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { KBQ_LUXON_DATE_ADAPTER_OPTIONS, LuxonDateAdapter } from '@koobiq/angular-luxon-adapter/adapter';
-import { DateAdapter, KBQ_DATE_LOCALE, KBQ_LOCALE_SERVICE } from '@koobiq/components/core';
+import { DateAdapter, KBQ_DATE_LOCALE, KBQ_LOCALE_SERVICE, KbqFormsModule } from '@koobiq/components/core';
 import { KbqDatepickerModule } from '@koobiq/components/datepicker';
 import { KbqFormFieldModule } from '@koobiq/components/form-field';
 import { KbqIconModule } from '@koobiq/components/icon';
 import { KbqInputModule } from '@koobiq/components/input';
 import { KbqRadioModule } from '@koobiq/components/radio';
+import { KbqTimepickerModule } from '@koobiq/components/timepicker';
 import { KbqToolTipModule } from '@koobiq/components/tooltip';
-import { DatepickerLanguageExample } from './datepicker-language/datepicker-language-example';
+import { DatepickerAndTimepickerExample } from './datepicker-and-timepicker/datepicker-and-timepicker-example';
+import { DatepickerInactiveExample } from './datepicker-inactive/datepicker-inactive-example';
 import { DatepickerMinimaxExample } from './datepicker-minimax/datepicker-minimax-example';
-import { DatepickerNotEmptyExample } from './datepicker-not-empty/datepicker-not-empty-example';
 import { DatepickerOverviewExample } from './datepicker-overview/datepicker-overview-example';
+import { DatepickerRangeExample } from './datepicker-range/datepicker-range-example';
+import { DatepickerRequiredExample } from './datepicker-required/datepicker-required-example';
 
-export { DatepickerLanguageExample, DatepickerMinimaxExample, DatepickerNotEmptyExample, DatepickerOverviewExample };
+export {
+    DatepickerAndTimepickerExample,
+    DatepickerInactiveExample,
+    DatepickerMinimaxExample,
+    DatepickerOverviewExample,
+    DatepickerRangeExample,
+    DatepickerRequiredExample
+};
 
 const EXAMPLES = [
-    DatepickerLanguageExample,
+    DatepickerAndTimepickerExample,
+    DatepickerInactiveExample,
     DatepickerOverviewExample,
-    DatepickerNotEmptyExample,
-    DatepickerMinimaxExample
+    DatepickerRequiredExample,
+    DatepickerMinimaxExample,
+    DatepickerRangeExample
 ];
 
 @NgModule({
@@ -33,7 +45,9 @@ const EXAMPLES = [
         KbqFormFieldModule,
         KbqIconModule,
         KbqRadioModule,
-        KbqToolTipModule
+        KbqToolTipModule,
+        KbqTimepickerModule,
+        KbqFormsModule
     ],
     declarations: EXAMPLES,
     exports: EXAMPLES,
