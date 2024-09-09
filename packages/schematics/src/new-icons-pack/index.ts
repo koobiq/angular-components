c975dbb2ee64b18945868621f479d6dbbef19c8d
Nikita Guryev <nikgurv@gmail.com>
2024-09-09 15:26:09 +0300

    feat(icon): script fix

--- packages/schematics/src/new-icons-pack/index.ts
+++ packages/schematics/src/new-icons-pack/index.ts
@@ -1,10 +1,14 @@
 import { Path } from '@angular-devkit/core';
 import { DirEntry, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import chalk from 'chalk';
+import * as path from 'path';
 
 import { LoggerApi } from '@angular-devkit/core/src/logger';
 import { setupOptions } from '../utils/package-config';
 import { ReplaceData, iconReplacements, newIconsPackData } from './data';
 import { Schema } from './schema';
+
+const { italic, blue, bold } = chalk;
 
 const data = newIconsPackData;
 
@@ -14,7 +18,7 @@ export default function newIconsPack(options: Schema): Rule {
     const pkg = '@koobiq/icons';
 
     return async (tree: Tree, context: SchematicContext) => {
-        const { project } = options;
+        const { project, fix, stylesExt } = options;
         try {
             const projectDefinition = await setupOptions(project, tree);
             targetDir = projectDefinition ? tree.getDir(projectDefinition.root) : tree;
@@ -24,13 +28,15 @@ export default function newIconsPack(options: Schema): Rule {
 
         const { logger } = context;
         const handleDeprecatedIcons = (newContent: string | undefined, path: Path) => {
-            if (options.fix) {
+            if (fix) {
                 data.forEach(({ replace, replaceWith }) => {
                     newContent = newContent!.replace(new RegExp(replace, 'g'), replaceWith);
                 });
             } else {
                 const foundIcons = data.filter(({ replace }) => newContent!.indexOf(replace) !== -1);
-                showWarning(path, foundIcons, logger);
+                if (foundIcons.length) {
+                    showWarning(path, foundIcons, logger);
+                }
             }
             return newContent;
         };
@@ -82,15 +88,13 @@ export default function newIconsPack(options: Schema): Rule {
                 return;
             }
 
-            if (path.endsWith(options.stylesExt)) {
+            if (path.endsWith(stylesExt)) {
                 const initialContent = entry?.content.toString();
                 let newContent = initialContent;
 
                 if (newContent) {
                     // replace icons in styles, update pkg name
-                    newContent = newContent
-                        .replace(new RegExp(`@koobiq/icons`, 'g'), pkg)
-                        .replace(new RegExp('mc-', 'g'), 'kbq-');
+                    newContent = newContent.replace(new RegExp('mc-', 'g'), 'kbq-');
 
                     newContent = handleDeprecatedIcons(newContent, path);
                 }
@@ -103,11 +107,10 @@ export default function newIconsPack(options: Schema): Rule {
     };
 }
 
-function showWarning(path: Path, foundIcons: ReplaceData[], logger: LoggerApi) {
-    if (foundIcons.length) {
-        logger.warn('-------------------------');
-        logger.warn(`Please pay attention! Found deprecated icons in file ${path}: `);
-        logger.warn(foundIcons.map(({ replace, replaceWith }) => `\t${replace} -> \t${replaceWith}`).join('\n'));
-        logger.warn('-------------------------');
-    }
+function showWarning(filePath: Path, foundIcons: ReplaceData[], logger: LoggerApi) {
+    logger.warn('-------------------------');
+    logger.warn(`Please pay attention! Found deprecated icons in file: `);
+    logger.info(`${bold(italic(blue(path.resolve(`.${filePath}`))))}`);
+    logger.warn(foundIcons.map(({ replace, replaceWith }) => `\t${replace} -> \t${replaceWith}`).join('\n'));
+    logger.warn('-------------------------');
 }
