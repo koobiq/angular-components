2223fb8eb16629e3a57ecf232d9fbedae3b70765
lskramarov <l.s.kramarov@gmail.com>
2024-09-23 13:03:19 +0300

    fix: docs:api-gen

--- tools/api-gen/extraction/index.ts
+++ tools/api-gen/extraction/index.ts
@@ -1,6 +1,7 @@
 import { NgtscProgram } from '@angular/compiler-cli';
 import { basename, join } from 'path';
 import ts from 'typescript';
+import { DocEntry } from '../rendering/entities';
 import { ClassEntryMetadata, EntryCollection, ModuleInfo, PackageMetadata } from '../types';
 import { src } from '../utils';
 import { entryHandler, prepareMergedMetadata, updateEntries } from './helpers';
@@ -72,11 +73,10 @@ export function extractApiToJson(packages: ModuleInfo[]) {
                     {}
                 );
 
-                const privateModules = new Set<string>([]);
                 return {
                     packageName,
                     entries: updateEntries(
-                        program.getApiDocumentation(resolvedPath, privateModules) as any,
+                        program.getApiDocumentation(resolvedPath, new Set<string>([])).entries as DocEntry[],
                         classesMetadata
                     )
                 };
