2589267eea553a1df15ce40081bce2f45e25d2a7
Artem Belik <artyombelyk@gmail.com>
2024-09-27 17:20:50 +0300

    feat(docs): removed core-js polyfills (#325)

--- apps/docs/src/main.ts
+++ apps/docs/src/main.ts
@@ -2,7 +2,6 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/';
 import { environment } from './environments/environment';
-import './polyfills';
 import { unregisterServiceWorkers } from './unregister-service-workers';
 
 // Unregister all installed service workers and force reload the page if there was
