f4e8badf28c088ecaff203457f38aaf85c7bb139
Aleksandr Knutov <sk8knut@gmail.com>
2024-10-09 12:33:16 +0300

    feat(docs): tabs examples (#DS-2907) (#329)

--- packages/docs-examples/components/tabs/tabs-vertical-icons/tabs-vertical-icons-example.ts
+++ packages/docs-examples/components/tabs/tabs-vertical-icons/tabs-vertical-icons-example.ts
@@ -2,14 +2,14 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { PopUpPlacements } from '@koobiq/components/core';
 
 /**
- * @title Tabs vertical
+ * @title Tabs vertical icons
  */
 @Component({
-    selector: 'tabs-vertical-example',
-    templateUrl: 'tabs-vertical-example.html',
-    styleUrls: ['tabs-vertical-example.css'],
+    selector: 'tabs-vertical-icons-example',
+    templateUrl: 'tabs-vertical-icons-example.html',
+    styleUrls: ['tabs-vertical-icons-example.css'],
     encapsulation: ViewEncapsulation.None
 })
-export class TabsVerticalExample {
+export class TabsVerticalIconsExample {
     protected readonly PopUpPlacements = PopUpPlacements;
 }
