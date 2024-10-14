d868f64bb8b766a7ab47d181858fb399e649c806
lskramarov <l.s.kramarov@gmail.com>
2024-10-14 17:24:57 +0300

    feat(code-block, timezone): localizability for code-block, timezone (#DS-2214)

--- packages/components/core/locales/zh-CN.ts
+++ packages/components/core/locales/zh-CN.ts
@@ -25,5 +25,18 @@ export const zhCNLocaleData = {
                 size: '大小'
             }
         }
+    },
+    codeBlock: {
+        softWrapOnTooltip: 'Включить перенос по словам',
+        softWrapOffTooltip: 'Выключить перенос по словам',
+        downloadTooltip: 'Скачать',
+        copiedTooltip: '✓ Скопировано',
+        copyTooltip: 'Скопировать',
+        viewAllText: 'Показать все',
+        viewLessText: 'Свернуть',
+        openExternalSystemTooltip: 'Открыть во внешней системе'
+    },
+    timezone: {
+        searchPlaceholder: 'Город или часовой пояс'
     }
 };
