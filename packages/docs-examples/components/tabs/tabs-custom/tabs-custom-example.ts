aadce791fc401febd5b5ffdcbec9c85dc7c2b52e
Aleksandr Knutov <sk8knut@gmail.com>
2024-10-15 11:39:54 +0300

    new custom tabs examples in docs

---
+++ packages/docs-examples/components/tabs/tabs-custom/tabs-custom-example.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+/**
+ * @title Tabs custom
+ */
+@Component({
+    selector: 'tabs-custom-example',
+    templateUrl: 'tabs-custom-example.html',
+    styleUrls: ['tabs-custom-example.css'],
+    encapsulation: ViewEncapsulation.None
+})
+export class TabsCustomExample {}
