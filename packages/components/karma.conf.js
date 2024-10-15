978caa2c055d990317604b27fc041c0b496b3755
Nikita Guryev <nikgurv@gmail.com>
2024-10-15 14:19:36 +0300

    fix(layout): removed map.get from properties used in styles (#DS-2972) (#365)

--- packages/components/karma.conf.js
+++ packages/components/karma.conf.js
@@ -4,6 +4,14 @@ module.exports = function (config) {
     const baseConfig = getBaseKarmaConfig();
     return config.set({
         ...baseConfig,
-        files: [{ pattern: '../../dist/components/prebuilt-themes/light-theme.css', included: true, watched: true }]
+        files: [
+            { pattern: '../../dist/components/prebuilt-themes/theme.css', included: true, watched: true },
+            { pattern: '../../node_modules/@koobiq/design-tokens/web/css-tokens.css', included: true, watched: true },
+            {
+                pattern: '../../node_modules/@koobiq/design-tokens/web/css-tokens-light.css',
+                included: true,
+                watched: true
+            }
+        ]
     });
 };
