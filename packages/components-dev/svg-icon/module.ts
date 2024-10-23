c9e12059dccbd4ec7b408619c5d146a2cade08f9
Aleksandr Knutov <sk8knut@gmail.com>
2024-10-23 20:11:10 +0300

    new version with registration svg

--- packages/components-dev/svg-icon/module.ts
+++ packages/components-dev/svg-icon/module.ts
@@ -3,6 +3,12 @@ import { ChangeDetectionStrategy, Component, NgModule, ViewEncapsulation } from 
 import { BrowserModule } from '@angular/platform-browser';
 import { KbqComponentColors } from '@koobiq/components/core';
 import { KbqSvgIconModule } from '../../components/svg-icon';
+import { KbqSvgIconRegistryService } from '../../components/svg-icon/svg-icon-registry.service';
+
+const folderOpen = require('@koobiq/icons/svg/folder-open_16.svg') as string;
+const chevronDownS = require('@koobiq/icons/svg/chevron-down-s_16.svg') as string;
+const calendarO = require('@koobiq/icons/svg/calendar-o_16.svg') as string;
+const clock = require('@koobiq/icons/svg/clock_16.svg') as string;
 
 @Component({
     selector: 'app',
@@ -13,6 +19,13 @@ import { KbqSvgIconModule } from '../../components/svg-icon';
 })
 export class DemoComponent {
     protected readonly colors = KbqComponentColors;
+
+    constructor(iconReg: KbqSvgIconRegistryService) {
+        iconReg.addSvg('folder-open', folderOpen);
+        iconReg.addSvg('chevron-down-s', chevronDownS);
+        iconReg.addSvg('calendar-o', calendarO);
+        iconReg.addSvg('clock', clock);
+    }
 }
 
 @NgModule({
