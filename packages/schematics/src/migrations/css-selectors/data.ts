e82713f71f61b674a460bdcce6b57ed4f066c6a0
Nikita Guryev <nikgurv@gmail.com>
2024-11-12 21:11:01 +0300

    feat(schematics): unit tests

--- packages/schematics/src/migrations/css-selectors/data.ts
+++ packages/schematics/src/migrations/css-selectors/data.ts
@@ -5,26 +5,26 @@ export interface ReplaceData {
 }
 
 export const cssSelectorsReplacement: ReplaceData[] = [
-    { replace: 'kbq-display-1', replaceWith: 'kbq-display-big' },
-    { replace: 'kbq-display-2', replaceWith: 'kbq-display-normal' },
-    { replace: 'kbq-display-3', replaceWith: 'kbq-display-compact' },
     { replace: 'kbq-display-1-strong', replaceWith: 'kbq-display-big-strong' },
     { replace: 'kbq-display-2-strong', replaceWith: 'kbq-display-normal-strong' },
     { replace: 'kbq-display-3-strong', replaceWith: 'kbq-display-compact-strong' },
-    { replace: 'kbq-body', replaceWith: 'kbq-text-big' },
     { replace: 'kbq-body-tabular', replaceWith: 'kbq-tabular-big' },
     { replace: 'kbq-body-strong', replaceWith: 'kbq-text-big-strong' },
-    { replace: 'kbq-body-caps', replaceWith: 'kbq-caps-big' },
-    { replace: 'kbq-body-mono', replaceWith: 'kbq-mono-big' },
     { replace: 'kbq-body-mono-strong', replaceWith: 'kbq-mono-big-strong' },
-    { replace: 'kbq-caption', replaceWith: 'kbq-text-normal' },
+    { replace: 'kbq-body-mono', replaceWith: 'kbq-mono-big' },
+    { replace: 'kbq-body-caps', replaceWith: 'kbq-caps-big' },
     { replace: 'kbq-caption-tabular', replaceWith: 'kbq-tabular-normal' },
     { replace: 'kbq-caption-strong', replaceWith: 'kbq-text-normal-strong' },
-    { replace: 'kbq-caption-caps', replaceWith: 'kbq-caps-normal' },
-    { replace: 'kbq-caption-mono', replaceWith: 'kbq-mono-normal' },
     { replace: 'kbq-caption-mono-strong', replaceWith: 'kbq-mono-normal-strong' },
-    { replace: 'kbq-extra-small-text', replaceWith: 'kbq-text-compact' },
+    { replace: 'kbq-caption-mono', replaceWith: 'kbq-mono-normal' },
+    { replace: 'kbq-caption-caps', replaceWith: 'kbq-caps-normal' },
     { replace: 'kbq-extra-small-text-strong', replaceWith: 'kbq-text-compact-strong' },
+    { replace: 'kbq-extra-small-text-mono', replaceWith: 'kbq-mono-compact' },
     { replace: 'kbq-extra-small-text-caps', replaceWith: 'kbq-caps-compact' },
-    { replace: 'kbq-extra-small-text-mono', replaceWith: 'kbq-mono-compact' }
+    { replace: 'kbq-display-1', replaceWith: 'kbq-display-big' },
+    { replace: 'kbq-display-2', replaceWith: 'kbq-display-normal' },
+    { replace: 'kbq-display-3', replaceWith: 'kbq-display-compact' },
+    { replace: 'kbq-body', replaceWith: 'kbq-text-big' },
+    { replace: 'kbq-caption', replaceWith: 'kbq-text-normal' },
+    { replace: 'kbq-extra-small-text', replaceWith: 'kbq-text-compact' }
 ];
--- packages/schematics/src/migrations/css-selectors/index.spec.ts
+++ packages/schematics/src/migrations/css-selectors/index.spec.ts
@@ -8,26 +8,23 @@ import { cssSelectorsReplacement } from './data';
 
 const collectionPath = path.join(__dirname, '../../collection.json');
 
-describe('new-icons-pack', () => {
+const SCHEMATIC_NAME = 'css-selectors';
+
+describe(SCHEMATIC_NAME, () => {
     let runner: SchematicTestRunner;
     let appTree: Tree;
     let projects: ProjectDefinitionCollection;
-    const elementsWithDeprecatedIconPrefixes = [
-        '<i kbq-icon="mc-word-wrap_16"></i>',
-        '<i kbq-icon-item="mc-word-wrap_16"></i>',
-        '<i kbq-icon-button="mc-word-wrap_16"></i>',
-        '<i class="mc kbq-icon mc-word-wrap_16"></i>',
-        '<div [class.mc-word-wrap_16]="true"></div>'
+    const elementsWithDeprecatedSelectors = [
+        '<div class="kbq-display-1"></div>',
+        '<div class="kbq-display-2"></div>',
+        '<div class="kbq-display-3"></div>',
+        '<div class="kbq-body"></div>',
+        '<div class="kbq-caption"></div>'
     ];
-    const updatedPkgName = '@koobiq/icons';
 
     beforeEach(async () => {
         runner = new SchematicTestRunner('schematics', collectionPath);
         appTree = await createTestApp(runner, { style: 'scss' });
-        const pkgPath = '/package.json';
-        const pkg = JSON.parse(appTree.read(pkgPath)!.toString());
-        pkg.dependencies['@koobiq/icons'] = '^9.1.0';
-        appTree.overwrite(pkgPath, JSON.stringify(pkg));
 
         const workspace = await getWorkspace(appTree);
         projects = workspace.projects as unknown as ProjectDefinitionCollection;
@@ -37,22 +34,10 @@ describe('new-icons-pack', () => {
 
             appTree.overwrite(
                 templatePath,
-                `${appTree.read(templatePath)!.toString()}\n${elementsWithDeprecatedIconPrefixes.join('\n')}`
+                `${appTree.read(templatePath)!.toString()}\n${elementsWithDeprecatedSelectors.join('\n')}`
             );
-            appTree.overwrite(stylesPath, `@use "@koobiq/icons"\n${appTree.read(stylesPath)!.toString()}`);
+            appTree.overwrite(stylesPath, '.kbq-display-1 {}\n' + '\n${appTree.read(stylesPath)!.toString()}');
         });
-    });
-
-    it('should skip changes if koobiq icons package with breaking changes is not installed', async () => {
-        const pkgPath = '/package.json';
-        const pkg = JSON.parse(appTree.read(pkgPath)!.toString());
-        delete pkg.dependencies['@koobiq/icons'];
-        appTree.overwrite(pkgPath, JSON.stringify(pkg));
-
-        runner.logger.subscribe(({ message }) =>
-            expect(message).toContain('Breaking version of icons is not used. Everything is OK.')
-        );
-        await runner.runSchematic('new-icons-pack', { fix: false }, appTree);
     });
 
     it('should run migration for specified project', async () => {
@@ -65,76 +50,77 @@ describe('new-icons-pack', () => {
             ];
         };
 
-        const tree = await runner.runSchematic('new-icons-pack', { fix: false, project: firstProjectKey }, appTree);
+        const tree = await runner.runSchematic(SCHEMATIC_NAME, { fix: true, project: firstProjectKey }, appTree);
 
         const [firstProjectTemplateContent, firstProjectStylesContent] = readProjectContent(firstProjectKey);
         expect(
-            elementsWithDeprecatedIconPrefixes.filter((item) => firstProjectTemplateContent.indexOf(item) !== -1).length
+            elementsWithDeprecatedSelectors.filter((item) => firstProjectTemplateContent.indexOf(item) !== -1).length
         ).toBeFalsy();
-        expect(firstProjectStylesContent).toContain(updatedPkgName);
+        expect(firstProjectStylesContent.includes('kbq-display-1')).toBeFalsy();
 
         const [secondProjectTemplateContent] = readProjectContent(secondProjectKey);
         expect(
-            elementsWithDeprecatedIconPrefixes.filter((item) => secondProjectTemplateContent.indexOf(item) !== -1)
-                .length
-        ).toEqual(elementsWithDeprecatedIconPrefixes.length);
+            elementsWithDeprecatedSelectors.filter((item) => secondProjectTemplateContent.indexOf(item) !== -1).length
+        ).toEqual(elementsWithDeprecatedSelectors.length);
     });
 
     it('should run migration for whole tree', async () => {
-        const tree = await runner.runSchematic('new-icons-pack', { fix: true }, appTree);
+        const tree = await runner.runSchematic(SCHEMATIC_NAME, { fix: true }, appTree);
 
         projects.forEach((project) => {
             const templateContent = tree.read(`/${project.root}/src/app/app.component.html`)!.toString();
 
             expect(
-                elementsWithDeprecatedIconPrefixes.filter((item) => templateContent.indexOf(item) !== -1).length
+                elementsWithDeprecatedSelectors.filter((item) => templateContent.indexOf(item) !== -1).length
             ).toBeFalsy();
         });
     });
 
-    it('should replace deprecated icons for fix = true', async () => {
-        const newIconsPackDataSlice = cssSelectorsReplacement.slice(0, 10);
-        const iconsToBeReplaced = newIconsPackDataSlice.map(({ replace }) => `<i kbq-icon="mc-${replace}"></i>`);
-        const iconsToBeReplacedWith = newIconsPackDataSlice.map(
-            ({ replaceWith }) => `<i kbq-icon="kbq-${replaceWith}"></i>`
-        );
+    it('should replace deprecated selectors for fix = true', async () => {
+        const selectorsToBeReplaced = cssSelectorsReplacement
+            .slice(0, 10)
+            .map(({ replace }) => `<div class="${replace}"></div>`);
+        const selectorsToBeReplacedWith = cssSelectorsReplacement
+            .slice(0, 10)
+            .map(({ replaceWith }) => `<div class="${replaceWith}"></div>`);
 
         projects.forEach((project) => {
             const templatePath = `/${project.root}/src/app/app.component.html`;
 
             appTree.overwrite(
                 templatePath,
-                `${appTree.read(templatePath)!.toString()}\n${iconsToBeReplaced.join('\n')}`
+                `${appTree.read(templatePath)!.toString()}\n${selectorsToBeReplaced.join('\n')}`
             );
         });
 
-        const tree = await runner.runSchematic('new-icons-pack', { fix: true }, appTree);
+        const tree = await runner.runSchematic(SCHEMATIC_NAME, { fix: true }, appTree);
 
         projects.forEach((project) => {
             const templateContent = tree.read(`/${project.root}/src/app/app.component.html`)!.toString();
 
-            expect(iconsToBeReplaced.filter((item) => templateContent.indexOf(item) !== -1).length).toBeFalsy();
-            expect(iconsToBeReplacedWith.filter((item) => templateContent.indexOf(item) !== -1).length).toEqual(
-                iconsToBeReplacedWith.length
+            expect(selectorsToBeReplaced.filter((item) => templateContent.indexOf(item) !== -1).length).toBeFalsy();
+            expect(selectorsToBeReplacedWith.filter((item) => templateContent.indexOf(item) !== -1).length).toEqual(
+                selectorsToBeReplacedWith.length
             );
         });
     });
 
-    it('should inform about deprecated icons for fix = false (default, without params)', async () => {
-        const newIconsPackDataSlice = cssSelectorsReplacement.slice(0, 10);
-        const iconsToBeReplaced = newIconsPackDataSlice.map(({ replace }) => `<i kbq-icon="mc-${replace}"></i>`);
+    it('should inform about deprecated selectors for fix = false (default, without params)', async () => {
+        const selectorsToBeReplaced = cssSelectorsReplacement
+            .slice(0, 10)
+            .map(({ replace }) => `<div class="${replace}"></div>`);
 
         projects.forEach((project) => {
             const templatePath = `/${project.root}/src/app/app.component.html`;
             appTree.overwrite(
                 templatePath,
-                `${appTree.read(templatePath)!.toString()}\n${iconsToBeReplaced.join('\n')}`
+                `${appTree.read(templatePath)!.toString()}\n${selectorsToBeReplaced.join('\n')}`
             );
         });
 
         // simply check for messages to be sent
         runner.logger.subscribe(({ message }) => expect(message).toBeTruthy());
 
-        await runner.runSchematic('new-icons-pack', {}, appTree);
+        await runner.runSchematic(SCHEMATIC_NAME, {}, appTree);
     });
 });
