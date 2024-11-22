7d7184686048a9636f8b1f3da7cd936ef9c527e1
Aleksandr Knutov <sk8knut@gmail.com>
2024-11-22 19:00:04 +0300

    chore(docs): datepicker examples fixes (#DS-3154)  (#445)

--- packages/docs-examples/components/datepicker/datepicker-and-timepicker/datepicker-and-timepicker-example.ts
+++ packages/docs-examples/components/datepicker/datepicker-and-timepicker/datepicker-and-timepicker-example.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { LuxonDateModule } from '@koobiq/angular-luxon-adapter/adapter';
+import { KbqFormsModule } from '@koobiq/components/core';
 import { KbqDatepickerModule } from '@koobiq/components/datepicker';
 import { KbqFormFieldModule } from '@koobiq/components/form-field';
+import { KbqIconModule } from '@koobiq/components/icon';
 import { KbqTimepickerModule } from '@koobiq/components/timepicker';
 import { DateTime } from 'luxon';
 
@@ -17,7 +19,9 @@ import { DateTime } from 'luxon';
         KbqDatepickerModule,
         KbqFormFieldModule,
         FormsModule,
-        LuxonDateModule
+        LuxonDateModule,
+        KbqFormsModule,
+        KbqIconModule
     ],
     template: `
         <div class="docs-example__datepicker-and-timepicker">
--- packages/docs-examples/components/datepicker/datepicker-range/datepicker-range-example.ts
+++ packages/docs-examples/components/datepicker/datepicker-range/datepicker-range-example.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { LuxonDateModule } from '@koobiq/angular-luxon-adapter/adapter';
-import { DateAdapter } from '@koobiq/components/core';
+import { DateAdapter, KbqFormsModule } from '@koobiq/components/core';
 import { KbqDatepickerModule } from '@koobiq/components/datepicker';
 import { KbqFormFieldModule } from '@koobiq/components/form-field';
 import { KbqIconModule } from '@koobiq/components/icon';
@@ -16,7 +16,8 @@ import { DateTime } from 'luxon';
         KbqDatepickerModule,
         KbqFormFieldModule,
         KbqIconModule,
-        LuxonDateModule
+        LuxonDateModule,
+        KbqFormsModule
     ],
     template: `
         <div class="docs-example__datepicker-range">
