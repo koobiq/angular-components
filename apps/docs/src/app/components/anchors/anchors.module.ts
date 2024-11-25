d5cd2b3b1376df3e0cd2b9f8f2dc8bb18aefa6df
Aleksandr Knutov <sk8knut@gmail.com>
2024-11-25 14:52:26 +0300

    add kbq title and styles

--- apps/docs/src/app/components/anchors/anchors.module.ts
+++ apps/docs/src/app/components/anchors/anchors.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AnchorsComponent } from './anchors.component';
 import { HeaderDirective } from './header.directive';
+import { KbqTitleModule } from '../../../../../../packages/components/title';
 
 @NgModule({
     imports: [
         RouterModule,
-        NgClass
+        NgClass,
+        KbqTitleModule
     ],
     exports: [AnchorsComponent, HeaderDirective],
     declarations: [AnchorsComponent, HeaderDirective]
