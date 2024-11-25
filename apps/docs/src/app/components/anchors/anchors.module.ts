3da585e4aea6381b369bfad0341be467f8c5af5c
Aleksandr Knutov <sk8knut@gmail.com>
2024-11-25 14:58:44 +0300

    linter fix

--- apps/docs/src/app/components/anchors/anchors.module.ts
+++ apps/docs/src/app/components/anchors/anchors.module.ts
@@ -1,9 +1,9 @@
 import { NgClass } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { KbqTitleModule } from '../../../../../../packages/components/title';
 import { AnchorsComponent } from './anchors.component';
 import { HeaderDirective } from './header.directive';
-import { KbqTitleModule } from '../../../../../../packages/components/title';
 
 @NgModule({
     imports: [
