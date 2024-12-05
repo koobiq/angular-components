18e791c9654642db283e5697b7e3d9f19fa213f0
Nikita Guryev <nikgurv@gmail.com>
2024-12-05 10:41:45 +0300

    feat(breadcrumbs): component init (#DS-2464)

---
+++ packages/components/breadcrumbs/breadcrumbs.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { BreadcrumbsComponent } from './breadcrumbs.component';
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [BreadcrumbsComponent],
+    providers: []
+})
+export class BreadcrumbsModule {}
