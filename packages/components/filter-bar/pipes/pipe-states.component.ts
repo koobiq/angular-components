807f8a8ef5c1ee9bcd52ebd557be7145185a909f
lskramarov <l.s.kramarov@gmail.com>
2024-12-18 16:39:45 +0300

    feat: states and multiple pipe

--- packages/components/filter-bar/pipes/pipe-states.component.ts
+++ packages/components/filter-bar/pipes/pipe-states.component.ts
@@ -1,50 +1,22 @@
-import { Directive, ElementRef, inject, Input, ViewChild } from '@angular/core';
+import { Directive, inject, Input, OnInit } from '@angular/core';
 import { KbqButton, KbqButtonStyles } from '@koobiq/components/button';
 import { KbqComponentColors } from '@koobiq/components/core';
 import { KbqPipe } from '../filter-bar.types';
+import { KbqPipeBase } from './pipe.component';
 
 @Directive({
     standalone: true,
-    selector: '[kbq-pipe-states]',
-    host: {
-        '[class]': 'computedStyles'
-    }
+    selector: '[kbq-pipe-states]'
 })
-export class KbqPipeStates {
+export class KbqPipeStates implements OnInit {
     private readonly button = inject(KbqButton);
+    private readonly basePipe = inject(KbqPipeBase);
 
-    protected computedStyles;
-
-    @ViewChild('kbqTitleText', { static: false }) textElement: ElementRef;
-
-    @Input()
+    @Input({ alias: 'kbq-pipe-states' })
     set pipe(pipe: KbqPipe | null) {
         this._pipe = pipe;
 
-        console.log('set pipe(value: KbqPipe | null) {: ');
-
-        this.computedStyles = '';
-        this.button.kbqStyle = KbqButtonStyles.Outline;
-        this.button.color = KbqComponentColors.ContrastFade;
-
-        if (this.pipe) {
-            if (this.pipe.readonly) {
-                this.button.kbqStyle = KbqButtonStyles.Outline;
-                this.button.color = KbqComponentColors.ContrastFade;
-            } else if (!this.pipe.empty) {
-                this.button.kbqStyle = KbqButtonStyles.Filled;
-                this.button.color = KbqComponentColors.ContrastFade;
-            }
-
-            this.computedStyles = {
-                'kbq-pipe_filled-pipe': this.pipe.changed
-            };
-
-            if (this.pipe.changed) {
-                this.button.kbqStyle = 'changed-filter';
-                this.button.color = KbqComponentColors.Empty;
-            }
-        }
+        this.updateState();
     }
 
     get pipe(): KbqPipe | null {
@@ -52,4 +24,18 @@ export class KbqPipeStates {
     }
 
     private _pipe: KbqPipe | null = null;
+
+    ngOnInit(): void {
+        this.basePipe.stateChanges.subscribe(this.updateState);
+    }
+
+    updateState = () => {
+        this.button.kbqStyle = KbqButtonStyles.Outline;
+        this.button.color = KbqComponentColors.ContrastFade;
+
+        if (!this.basePipe.isEmpty) {
+            this.button.kbqStyle = KbqButtonStyles.Filled;
+            this.button.color = KbqComponentColors.ContrastFade;
+        }
+    };
 }
