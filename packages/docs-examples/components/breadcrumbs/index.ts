74418b413cc77ffba8f7dc8dadf397b971619b10
Nikita Guryev <nikgurv@gmail.com>
2024-12-19 12:11:31 +0300

    feat(breadcrumbs): added examples and global customization options

--- packages/docs-examples/components/breadcrumbs/index.ts
+++ packages/docs-examples/components/breadcrumbs/index.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
+import { BreadcrumbsDropdownExample } from './breadcrumbs-dropdown/breadcrumbs-dropdown-example';
 import { BreadcrumbsOverviewExample } from './breadcrumbs-overview/breadcrumbs-overview-example';
 import { BreadcrumbsSizeExample } from './breadcrumbs-size/breadcrumbs-size-example';
 
-export { BreadcrumbsOverviewExample, BreadcrumbsSizeExample };
+export { BreadcrumbsDropdownExample, BreadcrumbsOverviewExample, BreadcrumbsSizeExample };
 
 const EXAMPLES = [
     BreadcrumbsOverviewExample,
-    BreadcrumbsSizeExample
+    BreadcrumbsSizeExample,
+    BreadcrumbsDropdownExample
 ];
 
 @NgModule({
