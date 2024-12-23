6cd98e27541f7e905ae5b4471fd0040211688fab
Nikita Guryev <nikgurv@gmail.com>
2024-12-23 11:24:02 +0300

    fix(modal): added modal-data token to public-api (#DS-3044)

--- packages/components/modal/public-api.ts
+++ packages/components/modal/public-api.ts
@@ -2,5 +2,5 @@ export { KbqModalRef } from './modal-ref.class';
 export { KbqModalComponent } from './modal.component';
 export * from './modal.directive';
 export { KbqModalModule } from './modal.module';
-export { KbqModalService } from './modal.service';
+export { KBQ_MODAL_DATA, KbqModalService } from './modal.service';
 export * from './modal.type';
