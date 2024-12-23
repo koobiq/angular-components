cebe646078a7dd5be2589fb837de015dbcde09d9
lskramarov <l.s.kramarov@gmail.com>
2024-12-23 11:17:34 +0300

    feat(docs): added example with user data in toast (#DS-3122)

---
+++ packages/docs-examples/components/toast/toast-user-data/toast-user-data-example.ts
@@ -0,0 +1,38 @@
+import { Component, inject, TemplateRef } from '@angular/core';
+import { KbqButtonModule } from '@koobiq/components/button';
+import { KbqLinkModule } from '@koobiq/components/link';
+import { KbqToastData, KbqToastModule, KbqToastService, KbqToastStyle } from '@koobiq/components/toast';
+
+/**
+ * @title Toast user data
+ */
+@Component({
+    standalone: true,
+    selector: 'toast-user-data-example',
+    imports: [
+        KbqLinkModule,
+        KbqButtonModule,
+        KbqToastModule
+    ],
+    templateUrl: 'toast-user-data-example.html'
+})
+export class ToastUserDataExample {
+    toastService = inject(KbqToastService);
+
+    showToast(actions: TemplateRef<any>) {
+        interface ExtendedUserData extends KbqToastData {
+            userData: string;
+            userButtonName: string;
+        }
+
+        this.toastService.show(<ExtendedUserData>{
+            style: KbqToastStyle.Success,
+            title: 'Доступно обновление агента',
+            caption: 'Версия 2.03.15 от 15 мая 2022',
+            actions,
+            closeButton: true,
+            userData: 'AnyUserData',
+            userButtonName: 'Обновить'
+        });
+    }
+}
