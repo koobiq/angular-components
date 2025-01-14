0c7dac7446f1958ddcb36c301d836ebc15f6516d
Nikita Guryev <nikgurv@gmail.com>
2025-01-14 11:03:40 +0300

    fix(docs): injected locale service for stackblitz example (#DS-3291) (#498)

--- apps/docs/src/assets/stackblitz/src/main.ts
+++ apps/docs/src/assets/stackblitz/src/main.ts
@@ -1,11 +1,12 @@
 import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { KBQ_LOCALE_SERVICE, KbqLocaleService } from '@koobiq/components/core';
 import { KoobiqDocsExample } from './example/koobiq-docs-example';
 
 bootstrapApplication(KoobiqDocsExample, {
     providers: [
         provideAnimations(),
-        provideHttpClient()
-    ]
+        provideHttpClient(),
+        { provide: KBQ_LOCALE_SERVICE, useClass: KbqLocaleService }]
 }).catch((err) => console.error(err));
