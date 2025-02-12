94da009ccbf0c3606b4fe5aee5c72e5fee80ef95
Nikita Guryev <nikgurv@gmail.com>
2025-02-12 12:14:35 +0300

    feat(top-menu): added new property to control hover shadow

--- packages/components/top-menu/top-menu.ts
+++ packages/components/top-menu/top-menu.ts
@@ -12,11 +12,19 @@ import { booleanAttribute, ChangeDetectionStrategy, Component, Input, ViewEncaps
     ],
     host: {
         class: 'kbq-top-menu',
-        '[class.kbq-top-menu-overflow]': 'hasOverflow'
+        '[class.kbq-top-menu-overflow]': 'hasOverflow',
+        '[class.kbq-top-menu-shadow-on-hover]': 'isShadowOnHoverEnabled'
     },
     changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None
 })
 export class KbqTopMenu {
+    /**
+     * Enables overflow behavior, applying `kbq-top-menu-overflow` to show a bottom shadow.
+     */
     @Input({ transform: booleanAttribute }) hasOverflow = false;
+    /**
+     * Enables shadow-on-hover, applying `kbq-top-menu-shadow-on-hover` for a bottom shadow on hover.
+     */
+    @Input({ transform: booleanAttribute }) isShadowOnHoverEnabled = true;
 }
