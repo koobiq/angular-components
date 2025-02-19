bc803b026e26bfbd834342a75d7f6a63c2174b23
Artem Belik <artyombelyk@gmail.com>
2025-02-19 12:00:24 +0300

    chore(dev): added DevLocaleSelector component

---
+++ packages/components-dev/locale-selector.ts
@@ -0,0 +1,24 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { KbqFormFieldModule } from '@koobiq/components-experimental/form-field';
+import { KbqLocaleService } from '@koobiq/components/core';
+import { KbqSelectModule } from '@koobiq/components/select';
+
+@Component({
+    standalone: true,
+    imports: [KbqFormFieldModule, KbqSelectModule, FormsModule],
+    selector: 'dev-locale-selector',
+    template: `
+        <kbq-form-field>
+            <kbq-select [ngModel]="localeService.id" (ngModelChange)="localeService.setLocale($event)">
+                @for (option of localeService.locales.items; track option.id) {
+                    <kbq-option [value]="option.id">{{ option.name }}</kbq-option>
+                }
+            </kbq-select>
+        </kbq-form-field>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DevLocaleSelector {
+    readonly localeService = inject(KbqLocaleService);
+}
