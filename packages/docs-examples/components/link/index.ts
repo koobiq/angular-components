260cf6e16bee330e1cd65e86190b4023f59fa012
Nikita Guryev <nikgurv@gmail.com>
2025-02-28 14:12:06 +0300

    fix(docs,link): removed color section (#DS-3398) (#551)

--- packages/docs-examples/components/link/index.ts
+++ packages/docs-examples/components/link/index.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { LinkApplicationExample } from './link-application/link-application-example';
 import { LinkCaptionExample } from './link-caption/link-caption-example';
-import { LinkColorExample } from './link-color/link-color-example';
 import { LinkDisabledExample } from './link-disabled/link-disabled-example';
 import { LinkExternalExample } from './link-external/link-external-example';
 import { LinkGeneralExample } from './link-general/link-general-example';
@@ -17,7 +16,6 @@ import { LinkVisitedExample } from './link-visited/link-visited-example';
 export {
     LinkApplicationExample,
     LinkCaptionExample,
-    LinkColorExample,
     LinkDisabledExample,
     LinkExternalExample,
     LinkGeneralExample,
@@ -42,7 +40,6 @@ const EXAMPLES = [
     LinkMultiLineExample,
     LinkPrepositionsExample,
     LinkIconsExample,
-    LinkColorExample,
     LinkVisitedExample,
     LinkDisabledExample,
     LinkCaptionExample
