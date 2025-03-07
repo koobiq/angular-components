7b0c7620329697d13f927bdd623369112d057fba
lskramarov <l.s.kramarov@gmail.com>
2025-03-07 19:17:15 +0300

    fix: svgPreview

--- apps/docs/src/app/services/documentation-items.ts
+++ apps/docs/src/app/services/documentation-items.ts
@@ -344,7 +344,7 @@ const DOCS: { [key: string]: DocCategory[] } = {
                         ru: 'Filter Bar',
                         en: 'Filter Bar'
                     },
-                    svgPreview: '',
+                    svgPreview: 'filter-bar',
                     hasApi: true,
                     apiId: 'filter-bar',
                     hasExamples: true
