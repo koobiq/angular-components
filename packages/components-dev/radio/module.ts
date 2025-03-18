a456ad4a6a64a69e86d056ec33d549fc378decce
Artem Belik <artyombelyk@gmail.com>
2025-03-18 16:43:59 +0300

    chore(dev): migrated to standalone radio app (#586)

--- packages/components-dev/radio/module.ts
+++ packages/components-dev/radio/module.ts
@@ -1,48 +1,60 @@
-import { Component, NgModule, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { ThemePalette } from '@koobiq/components/core';
 import { KbqFormFieldModule } from '@koobiq/components/form-field';
 import { KbqRadioChange, KbqRadioModule } from '@koobiq/components/radio';
+import { RadioExamplesModule } from 'packages/docs-examples/components/radio';
 
 @Component({
-    selector: 'app',
-    styleUrls: ['./styles.scss'],
-    encapsulation: ViewEncapsulation.None,
-    templateUrl: './template.html'
+    standalone: true,
+    imports: [RadioExamplesModule],
+    selector: 'dev-radio-examples',
+    template: `
+        <radio-size-example />
+        <hr />
+        <radio-style-example />
+        <hr />
+        <radio-content-example />
+        <hr />
+        <radio-group-example />
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DemoComponent {
-    themePalette = ThemePalette;
+export class DevRadioExamples {}
+
+@Component({
+    standalone: true,
+    imports: [
+        FormsModule,
+        KbqFormFieldModule,
+        KbqRadioModule,
+        DevRadioExamples
+    ],
+    selector: 'app',
+    templateUrl: './template.html',
+    styleUrl: './styles.scss',
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RadioDev {
+    readonly themePalette = ThemePalette;
 
     favoriteFruit: string;
 
     isDisabled: boolean = true;
 
-    fruits = [
+    readonly fruits = [
         'Apple',
         'Banana',
-        'Tomato',
-        'Крякать как уточка'
+        'Tomato'
     ];
 
-    selectionList = [
+    readonly selectionList = [
         { name: 'Yes', value: 'true', selected: false },
         { name: 'No', value: 'false', selected: true }
     ];
 
-    onChange($event: KbqRadioChange) {
+    onChange($event: KbqRadioChange): void {
         console.log('KbqRadioChange: ', $event);
     }
 }
-
-@NgModule({
-    declarations: [DemoComponent],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        KbqFormFieldModule,
-        KbqRadioModule
-    ],
-    bootstrap: [DemoComponent]
-})
-export class DemoModule {}
