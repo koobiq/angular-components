ac492eb482f249f5a97611269b9e7857ab02a86a
Nikita Guryev <nikgurv@gmail.com>
2025-03-21 10:53:42 +0300

    feat(top-bar): updated docs examples (#DS-3487)

--- packages/docs-examples/components/top-bar/top-bar-overflow/top-bar-overflow-example.ts
+++ packages/docs-examples/components/top-bar/top-bar-overflow/top-bar-overflow-example.ts
@@ -1,6 +1,5 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { CdkScrollable } from '@angular/cdk/overlay';
-import { AsyncPipe } from '@angular/common';
 import {
     AfterViewInit,
     ChangeDetectionStrategy,
@@ -11,8 +10,10 @@ import {
     WritableSignal
 } from '@angular/core';
 import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { KbqBadgeModule } from '@koobiq/components/badge';
 import { KbqButtonModule, KbqButtonStyles } from '@koobiq/components/button';
 import { KbqComponentColors, PopUpPlacements } from '@koobiq/components/core';
+import { KbqDlModule } from '@koobiq/components/dl';
 import { KbqIconModule } from '@koobiq/components/icon';
 import { KbqToolTipModule } from '@koobiq/components/tooltip';
 import { KbqTopBarModule } from '@koobiq/components/top-bar';
@@ -25,21 +26,23 @@ import { auditTime, map } from 'rxjs/operators';
     standalone: true,
     selector: 'top-bar-overflow-example',
     imports: [
-        AsyncPipe,
         CdkScrollable,
         KbqTopBarModule,
         KbqButtonModule,
         KbqToolTipModule,
-        KbqIconModule
+        KbqIconModule,
+        KbqDlModule,
+        KbqBadgeModule
     ],
     changeDetection: ChangeDetectionStrategy.OnPush,
     template: `
         <kbq-top-bar [withShadow]="hasOverflow()">
             <div class="layout-align-center-center" kbqTopBarContainer placement="start">
-                <div class="layout-row layout-padding-m flex-none">
-                    <i class="layout-row flex" kbq-icon="kbq-dashboard_16"></i>
+                <div class="kbq-title example-kbq-top-bar__title">
+                    <span class="kbq-truncate-line layout-margin-right-xs">Page</span>
+
+                    <span class="example-kbq-top-bar__counter">13 294</span>
                 </div>
-                <div class="kbq-title kbq-truncate-line">Dashboard</div>
             </div>
             <div kbqTopBarSpacer></div>
             <div kbqTopBarContainer placement="end">
@@ -63,23 +66,80 @@ import { auditTime, map } from 'rxjs/operators';
             </div>
         </kbq-top-bar>
         <div class="overflow-content-example kbq-scrollbar" cdkScrollable>
-            <div style="height: 600px">Scroll down ⬇️ to see box-shadow</div>
+            <div style="height: 600px">
+                <kbq-dl>
+                    <kbq-dt>Описание</kbq-dt>
+                    <kbq-dd>
+                        В НАСА описывают случай, когда человек случайно оказался в пространстве, близком к вакууму
+                        (давление ниже 1 Па) из-за утечки воздуха из скафандра.
+                    </kbq-dd>
+
+                    <kbq-dt>Статус</kbq-dt>
+                    <kbq-dd><kbq-badge>Значок</kbq-badge></kbq-dd>
+
+                    <kbq-dt>Статус</kbq-dt>
+                    <kbq-dd>Новый</kbq-dd>
+
+                    <kbq-dt>Дополнительно</kbq-dt>
+                    <kbq-dd>
+                        Космос — это бескрайнее пространство, полное загадок и чудес. Он начинается за пределами нашей
+                        атмосферы и простирается до самых дальних уголков Вселенной. В космосе находятся миллиарды
+                        звезд, планет и галактик, каждая из которых имеет свою уникальную историю и характеристики.
+                        Исследование космоса помогает нам лучше понять не только саму Вселенную, но и наше место в ней.
+                        <br />
+                        <br />
+                        Одним из самых захватывающих аспектов космоса является его бесконечность. Ученые до сих пор не
+                        могут точно определить, где заканчивается Вселенная. Существуют теории о том, что она может быть
+                        бесконечной или же иметь определенные границы. Это открывает множество вопросов о том, что
+                        находится за пределами видимого космоса и какие формы жизни могут существовать на других
+                        планетах. Космические исследования также играют важную роль в развитии технологий на Земле.
+                        Многие изобретения, которые мы используем в повседневной жизни, были разработаны благодаря
+                        космическим программам. Например, спутниковая навигация, системы связи и даже некоторые
+                        медицинские технологии имеют свои корни в космических исследованиях. Это показывает, как
+                        изучение космоса может приносить пользу человечеству в целом. Наконец, космос вдохновляет людей
+                        на творчество и мечты. С детства мы смотрим на звезды и задаемся вопросами о том, что может быть
+                        за пределами нашей планеты. Фильмы, книги и искусство часто черпают вдохновение из космоса,
+                        создавая образы, которые захватывают воображение. Это стремление к исследованию и открытию
+                        нового делает космос не только научной, но и культурной частью нашей жизни.
+                    </kbq-dd>
+                </kbq-dl>
+            </div>
         </div>
     `,
     styles: `
+        ::ng-deep .docs-live-example__example_top-bar-overflow {
+            background: var(--kbq-background-bg-secondary);
+            padding: var(--kbq-size-3xl);
+        }
+
         :host {
             display: flex;
             flex-direction: column;
             height: 400px;
+            border-radius: var(--kbq-size-border-radius);
+            border: 1px solid var(--kbq-line-contrast-less);
+            background: var(--kbq-background-bg);
+        }
+
+        :host .kbq-top-bar {
+            border-radius: var(--kbq-size-border-radius) var(--kbq-size-border-radius) 0 0;
         }
 
         .overflow-content-example {
             height: 100%;
             overflow-y: auto;
             scroll-behavior: smooth;
-            border: 1px solid var(--kbq-line-contrast-less);
-            border-top: 0;
-            border-radius: 0 0 var(--kbq-size-border-radius) var(--kbq-size-border-radius);
+            padding: 0 var(--kbq-size-xxl);
+        }
+
+        .example-kbq-top-bar__counter {
+            color: var(--kbq-foreground-contrast-tertiary);
+            flex: 1;
+        }
+
+        .example-kbq-top-bar__title {
+            display: inline-flex;
+            white-space: nowrap;
         }
     `
 })
