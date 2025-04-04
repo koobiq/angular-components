80ebc9cc659926d95bab25628a3935e17c4fce09
lskramarov <l.s.kramarov@gmail.com>
2025-04-04 21:27:55 +0300

    fix: delete import

--- packages/components/checkbox/checkbox-module.ts
+++ packages/components/checkbox/checkbox-module.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { KbqColorDirective } from '@koobiq/components/core';
 import { KbqCheckbox } from './checkbox';
 import { KbqCheckboxRequiredValidator } from './checkbox-required-validator';
 
 @NgModule({
-    imports: [CommonModule, KbqColorDirective],
+    imports: [CommonModule],
     exports: [KbqCheckbox, KbqCheckboxRequiredValidator],
     declarations: [KbqCheckbox, KbqCheckboxRequiredValidator]
 })
