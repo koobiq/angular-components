80fc69cec34de6bb623f478627d570ca755dd083
Nikita Guryev <nikgurv@gmail.com>
2025-04-06 14:00:53 +0300

    chore: fix tests

--- packages/schematics/src/migrations/empty-state-size-attr/index.spec.ts
+++ packages/schematics/src/migrations/empty-state-size-attr/index.spec.ts
@@ -1,16 +1,27 @@
+import { ProjectDefinitionCollection } from '@angular-devkit/core/src/workspace';
+import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { getWorkspace } from '@schematics/angular/utility/workspace';
 import * as path from 'path';
+import { createTestApp } from '../../utils/testing';
+import { Schema } from './schema';
 
 const collectionPath = path.join(__dirname, '../../collection.json');
 const SCHEMATIC_NAME = 'empty-state-size-attr';
 
 describe(SCHEMATIC_NAME, () => {
     let runner: SchematicTestRunner;
-    beforeEach(async () => {
-        runner = new SchematicTestRunner('schematics', collectionPath);
-    });
+    let appTree: Tree;
+    let projects: ProjectDefinitionCollection;
 
     it('should run migration for specified project', async () => {
-        await runner.runSchematic(SCHEMATIC_NAME);
+        runner = new SchematicTestRunner('schematics', collectionPath);
+        appTree = await createTestApp(runner, { style: 'scss' });
+
+        const workspace = await getWorkspace(appTree);
+        projects = workspace.projects as unknown as ProjectDefinitionCollection;
+
+        const [firstProjectKey] = projects.keys();
+        await runner.runSchematic(SCHEMATIC_NAME, { project: firstProjectKey } satisfies Schema, appTree);
     });
 });
