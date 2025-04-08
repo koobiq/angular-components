e80ea524a9e6c7f2ede0ecc9aa0d618af74b73d3
Nikita Guryev <nikgurv@gmail.com>
2025-04-08 19:30:28 +0300

    feat(schematics): added loader-overlay migration new property schematic (#DS-3594)

---
+++ packages/schematics/src/migrations/loader-overlay-size-attr/index.spec.ts
@@ -0,0 +1,71 @@
+import { ProjectDefinitionCollection } from '@angular-devkit/core/src/workspace';
+import { Tree } from '@angular-devkit/schematics';
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { ProjectDefinition } from '@schematics/angular/utility';
+import { getWorkspace } from '@schematics/angular/utility/workspace';
+import * as path from 'path';
+import { createTestApp } from '../../utils/testing';
+import { Schema } from './schema';
+
+const collectionPath = path.join(__dirname, '../../collection.json');
+const SCHEMATIC_NAME = 'loader-overlay-size-attr';
+
+const getProjectContentPaths = (project: ProjectDefinition) => {
+    return {
+        templatePath: `/${project.root}/src/app/app.component.html`,
+        tsPath: `/${project.root}/src/app/app.component.ts`,
+        stylesPath: `/${project.root}/src/styles.scss`
+    };
+};
+
+describe(SCHEMATIC_NAME, () => {
+    let runner: SchematicTestRunner;
+    let appTree: Tree;
+    let projects: ProjectDefinitionCollection;
+
+    beforeEach(async () => {
+        runner = new SchematicTestRunner('schematics', collectionPath);
+        appTree = await createTestApp(runner, { style: 'scss' });
+
+        const workspace = await getWorkspace(appTree);
+        projects = workspace.projects as unknown as ProjectDefinitionCollection;
+        projects.forEach((project) => {
+            const { templatePath } = getProjectContentPaths(project);
+
+            const template =
+                '<div>' +
+                '<kbq-loader-overlay [compact]="false"></kbq-loader-overlay>' +
+                '<kbq-loader-overlay [compact]="true"></kbq-loader-overlay>' +
+                '</div>';
+
+            appTree.overwrite(templatePath, template);
+        });
+    });
+
+    it('should run migration for specified project', async () => {
+        runner = new SchematicTestRunner('schematics', collectionPath);
+        appTree = await createTestApp(runner, { style: 'scss' });
+
+        const workspace = await getWorkspace(appTree);
+        projects = workspace.projects as unknown as ProjectDefinitionCollection;
+
+        const [firstProjectKey] = projects.keys();
+        await runner.runSchematic(SCHEMATIC_NAME, { project: firstProjectKey } satisfies Schema, appTree);
+    });
+
+    it('should run migration for external html', async () => {
+        const workspace = await getWorkspace(appTree);
+        projects = workspace.projects as unknown as ProjectDefinitionCollection;
+
+        const [firstProjectKey] = projects.keys();
+        const { templatePath } = getProjectContentPaths(projects.get(firstProjectKey)!);
+
+        const updatedTree = await runner.runSchematic(
+            SCHEMATIC_NAME,
+            { project: firstProjectKey } satisfies Schema,
+            appTree
+        );
+
+        expect(updatedTree.read(templatePath)?.toString()).toMatchSnapshot(`project ${firstProjectKey}: after changes`);
+    });
+});
---
+++ packages/schematics/src/migrations/loader-overlay-size-attr/index.ts
@@ -0,0 +1,63 @@
+import { SchematicContext, Tree } from '@angular-devkit/schematics';
+import { resolve } from 'node:path';
+import * as process from 'node:process';
+import ts from 'typescript';
+import { migrateTemplate, migrateTs } from '../../utils/angular-parsing';
+import { getParsingInfo } from '../../utils/package-config';
+import { canMigrateFile, MigrationData } from '../../utils/typescript';
+import { Schema } from './schema';
+
+export const migrationData: MigrationData = {
+    elementName: 'kbq-loader-overlay',
+    attrs: {
+        key: {
+            from: 'compact',
+            to: 'size'
+        },
+        value: {
+            replacements: [
+                { from: 'true', to: 'compact' },
+                { from: 'false', to: 'big' }
+            ],
+            default: 'big'
+        }
+    }
+};
+
+export default function migrate(options: Schema) {
+    return async (tree: Tree, context: SchematicContext) => {
+        const { project } = options;
+        const { tsPaths, templatePaths, projectDefinition } = await getParsingInfo(project, tree);
+
+        try {
+            const program = ts.createProgram(
+                Array.from(tsPaths, (item) => resolve(projectDefinition.root, `.${item}`)),
+                {
+                    baseUrl: projectDefinition.root,
+                    rootDir: projectDefinition.root,
+                    _enableTemplateTypeChecker: true, // Required for the template type checker to work.
+                    compileNonExportedClasses: true, // We want to migrate non-exported classes too.
+                    // Avoid checking libraries to speed up the migration.
+                    skipLibCheck: true,
+                    skipDefaultLibCheck: true
+                }
+            );
+
+            // Update external html
+            await migrateTemplate(tree, Array.from(templatePaths), context, migrationData);
+
+            // Update inline html
+            await migrateTs(
+                tree,
+                program.getSourceFiles().filter((sourceFile) => canMigrateFile(sourceFile, program)),
+                process.cwd(),
+                context,
+                migrationData
+            );
+
+            context.logger.warn('Warning! Run linter in updated files since line breaks or indents maybe be broken.');
+        } catch (e) {
+            context.logger.error(e as any);
+        }
+    };
+}
