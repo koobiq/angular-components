efa3bc28c6185be1abfcfec75328091302403181
Nikita Guryev <nikgurv@gmail.com>
2025-04-10 13:15:08 +0300

    chore: fixed unit tests

--- packages/schematics/src/migrations/loader-overlay-size-attr/index.spec.ts
+++ packages/schematics/src/migrations/loader-overlay-size-attr/index.spec.ts
@@ -1,7 +1,7 @@
 import { ProjectDefinitionCollection } from '@angular-devkit/core/src/workspace';
 import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
-import { ProjectDefinition } from '@schematics/angular/utility';
+import { ProjectDefinition, WorkspaceDefinition } from '@schematics/angular/utility';
 import { getWorkspace } from '@schematics/angular/utility/workspace';
 import * as path from 'path';
 import { createTestApp } from '../../utils/testing';
@@ -22,22 +22,21 @@ describe(SCHEMATIC_NAME, () => {
     let runner: SchematicTestRunner;
     let appTree: Tree;
     let projects: ProjectDefinitionCollection;
+    let workspace: WorkspaceDefinition;
 
-    it('should run migration for specified project', async () => {
+    beforeEach(async () => {
         runner = new SchematicTestRunner('schematics', collectionPath);
         appTree = await createTestApp(runner, { style: 'scss' });
-
-        const workspace = await getWorkspace(appTree);
+        workspace = await getWorkspace(appTree);
         projects = workspace.projects as unknown as ProjectDefinitionCollection;
+    });
 
+    it('should run migration for specified project', async () => {
         const [firstProjectKey] = projects.keys();
         await runner.runSchematic(SCHEMATIC_NAME, { project: firstProjectKey } satisfies Schema, appTree);
     });
 
     it('should run migration for external html', async () => {
-        const workspace = await getWorkspace(appTree);
-        projects = workspace.projects as unknown as ProjectDefinitionCollection;
-
         const [firstProjectKey] = projects.keys();
         const { templatePath } = getProjectContentPaths(projects.get(firstProjectKey)!);
 
@@ -60,12 +59,6 @@ describe(SCHEMATIC_NAME, () => {
 
     it('should throw message if replaced attr value is not static', async () => {
         const warnSpy = jest.spyOn(console, 'warn').mockImplementation();
-
-        runner = new SchematicTestRunner('schematics', collectionPath);
-        appTree = await createTestApp(runner, { style: 'scss' });
-
-        const workspace = await getWorkspace(appTree);
-        projects = workspace.projects as unknown as ProjectDefinitionCollection;
         const [firstProjectKey] = projects.keys();
         const { templatePath } = getProjectContentPaths(projects.get(firstProjectKey)!);
 
