7aa75a841306d05221abb15c36d95b46b779d31e
Nikita Guryev <nikgurv@gmail.com>
2025-04-10 13:13:25 +0300

    chore: added unit tests for case when value is not static

--- packages/schematics/src/migrations/empty-state-size-attr/index.spec.ts
+++ packages/schematics/src/migrations/empty-state-size-attr/index.spec.ts
@@ -1,7 +1,7 @@
 import { ProjectDefinitionCollection } from '@angular-devkit/core/src/workspace';
 import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
-import { ProjectDefinition } from '@schematics/angular/utility';
+import { ProjectDefinition, WorkspaceDefinition } from '@schematics/angular/utility';
 import { getWorkspace } from '@schematics/angular/utility/workspace';
 import * as path from 'path';
 import { createTestApp } from '../../utils/testing';
@@ -22,43 +22,30 @@ describe(SCHEMATIC_NAME, () => {
     let runner: SchematicTestRunner;
     let appTree: Tree;
     let projects: ProjectDefinitionCollection;
+    let workspace: WorkspaceDefinition;
 
     beforeEach(async () => {
         runner = new SchematicTestRunner('schematics', collectionPath);
         appTree = await createTestApp(runner, { style: 'scss' });
-
-        const workspace = await getWorkspace(appTree);
+        workspace = await getWorkspace(appTree);
         projects = workspace.projects as unknown as ProjectDefinitionCollection;
-        projects.forEach((project) => {
-            const { templatePath } = getProjectContentPaths(project);
-
-            const template =
-                '<div>' +
-                '<kbq-empty-state [big]="false"></kbq-empty-state>' +
-                '<kbq-empty-state [big]="true"></kbq-empty-state>' +
-                '</div>';
-
-            appTree.overwrite(templatePath, template);
-        });
     });
 
     it('should run migration for specified project', async () => {
-        runner = new SchematicTestRunner('schematics', collectionPath);
-        appTree = await createTestApp(runner, { style: 'scss' });
-
-        const workspace = await getWorkspace(appTree);
-        projects = workspace.projects as unknown as ProjectDefinitionCollection;
-
         const [firstProjectKey] = projects.keys();
         await runner.runSchematic(SCHEMATIC_NAME, { project: firstProjectKey } satisfies Schema, appTree);
     });
 
     it('should run migration for external html', async () => {
-        const workspace = await getWorkspace(appTree);
-        projects = workspace.projects as unknown as ProjectDefinitionCollection;
-
         const [firstProjectKey] = projects.keys();
         const { templatePath } = getProjectContentPaths(projects.get(firstProjectKey)!);
+
+        const template =
+            '<div>' +
+            '<kbq-empty-state [big]="false"></kbq-empty-state>' +
+            '<kbq-empty-state [big]="true"></kbq-empty-state>' +
+            '</div>';
+        appTree.overwrite(templatePath, template);
 
         const updatedTree = await runner.runSchematic(
             SCHEMATIC_NAME,
@@ -67,5 +54,25 @@ describe(SCHEMATIC_NAME, () => {
         );
 
         expect(updatedTree.read(templatePath)?.toString()).toMatchSnapshot(`project ${firstProjectKey}: after changes`);
+    });
+
+    it('should throw message if replaced attr value is not static', async () => {
+        const warnSpy = jest.spyOn(console, 'warn').mockImplementation();
+        const [firstProjectKey] = projects.keys();
+        const { templatePath } = getProjectContentPaths(projects.get(firstProjectKey)!);
+
+        // Set up the template with a non-static attribute value
+        const nonStaticTemplate = '<div><kbq-empty-state [big]="VARIABLE"></kbq-empty-state></div>';
+        appTree.overwrite(templatePath, nonStaticTemplate);
+        const templateBeforeUpdate = appTree.read(templatePath)?.toString();
+
+        const updatedTree = await runner.runSchematic(
+            SCHEMATIC_NAME,
+            { project: firstProjectKey } satisfies Schema,
+            appTree
+        );
+
+        expect(templateBeforeUpdate).toBe(updatedTree.read(templatePath)?.toString());
+        expect(warnSpy.mock.calls.some(([msg]) => msg.includes(templatePath))).toBe(true);
     });
 });
