a865252affee0d1953063b0272f7e86d38232bf5
lskramarov <lskramarov@users.noreply.github.com>
2025-04-22 12:36:54 +0300

    fix(filter-bar): formatting date ranges (#DS-3689) (#676)

--- packages/components/core/formatters/date/formatter.ts
+++ packages/components/core/formatters/date/formatter.ts
@@ -1,16 +1,16 @@
-import { Inject, Injectable, Optional } from '@angular/core';
+import { inject, Inject, Injectable } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { DateAdapter } from '@koobiq/date-adapter';
 import { DateFormatter as BaseDateFormatter } from '@koobiq/date-formatter';
 import { KBQ_DATE_LOCALE } from '../../datetime';
-import { KBQ_LOCALE_SERVICE } from '../../locales';
+import { KBQ_LOCALE_SERVICE, KbqLocaleService } from '../../locales';
 
 @Injectable()
 export class DateFormatter<D> extends BaseDateFormatter<D> {
+    protected localeService = inject<KbqLocaleService>(KBQ_LOCALE_SERVICE, { optional: true });
     constructor(
         override readonly adapter: DateAdapter<D>,
-        @Inject(KBQ_DATE_LOCALE) locale: string,
-        @Optional() @Inject(KBQ_LOCALE_SERVICE) protected localeService?
+        @Inject(KBQ_DATE_LOCALE) locale: string
     ) {
         super(adapter, locale);
 
