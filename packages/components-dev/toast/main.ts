9d4a870013932f4429e20bb837acc6a466c2868c
Nikita Guryev <nikgurv@gmail.com>
2025-04-30 14:12:15 +0300

    fix(toast): adjust margin when used custom position (#DS-3370) (#714)

--- packages/components-dev/toast/main.ts
+++ packages/components-dev/toast/main.ts
@@ -1,9 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { KBQ_TOAST_CONFIG, KbqToastConfig, KbqToastPosition } from '@koobiq/components/toast';
 import { ToastDev } from './module';
 
 bootstrapApplication(ToastDev, {
     providers: [
-        provideAnimations()
+        provideAnimations(),
+        {
+            provide: KBQ_TOAST_CONFIG,
+            useValue: {
+                position: KbqToastPosition.TOP_RIGHT,
+                duration: 5000,
+                delay: 2000,
+                onTop: false
+            } satisfies KbqToastConfig
+        }
     ]
 }).catch((error) => console.error(error));
