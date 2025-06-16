2b2f32e82fbf16fbfeba00416af77160e4f2b24d
Artem Belik <artyombelyk@gmail.com>
2025-06-16 13:56:43 +0300

    style: setup stylelint prefix validation for packages (#DS-3497)

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,26 +1,93 @@
 // @ts-check
 
-const KEBAB_CASE_PATTERN = '^(_?[a-z][a-z0-9]*)(-[a-z0-9]+)*$';
+const kebabCasePattern = '^(_?[a-z][a-z0-9]*)(-[a-z0-9]+)*$';
 
-const ALLOWED_PREFIXES = [
-    // Default
-    'kbq-',
-    // packages/docs-examples
-    'example-',
-    // packages/components-dev
-    'dev-',
-    // apps/docs
-    'docs-',
-    // Angular specific
-    'cdk-',
-    // Angular specific
-    'ng-',
-    // @docsearch/css
-    'DocSearch-',
-    // highlight.js
-    'hljs-',
-    // overlayscrollbars
-    'os-'
+/**
+ * @see https://stylelint.io/user-guide/rules/selector-class-pattern
+ *
+ * @param {string[]} prefixes
+ */
+const makeSelectorClassPatternOptions = (prefixes = []) => {
+    const allowedPrefixes = [
+        // Default
+        'kbq-',
+        // Angular specific
+        'cdk-',
+        // Angular specific
+        'ng-'
+    ];
+
+    return [
+        `^_?(${allowedPrefixes.concat(prefixes).join('|')})`,
+        {
+            resolveNestedSelectors: true
+        }
+    ];
+};
+
+/**
+ * Override rules for packages/components
+ *
+ * @type {import('stylelint').Config['overrides']}
+ */
+const componentsRules = [
+    {
+        files: ['packages/components/**/*.scss', 'packages/components/**/*.css'],
+        rules: {
+            'selector-class-pattern': makeSelectorClassPatternOptions([
+                // highlight.js
+                'hljs-',
+                // overlayscrollbars
+                'os-'
+            ])
+        }
+    }
+];
+
+/**
+ * Override rules for packages/components-dev
+ *
+ * @type {import('stylelint').Config['overrides']}
+ */
+const componentsDevRules = [
+    {
+        files: ['packages/components-dev/**/*.scss', 'packages/components-dev/**/*.css'],
+        rules: {
+            'selector-class-pattern': makeSelectorClassPatternOptions(['dev-'])
+        }
+    }
+];
+
+/**
+ * Override rules for packages/docs-examples
+ *
+ * @type {import('stylelint').Config['overrides']}
+ */
+const componentsExamplesRules = [
+    {
+        files: ['packages/docs-examples/**/*.scss', 'packages/docs-examples/**/*.css'],
+        rules: {
+            'selector-class-pattern': makeSelectorClassPatternOptions(['example-'])
+        }
+    }
+];
+
+/**
+ * Override rules for apps/docs
+ *
+ * @type {import('stylelint').Config['overrides']}
+ */
+const docsRules = [
+    {
+        files: ['apps/docs/**/*.scss', 'apps/docs/**/*.css'],
+        rules: {
+            'selector-class-pattern': makeSelectorClassPatternOptions([
+                'docs-',
+                // @docsearch/css
+                'DocSearch-'
+            ])
+        }
+    }
 ];
 
 /** @type {import('stylelint').Config} */
@@ -59,19 +126,20 @@ const config = {
                 ]
             }
         ],
-        'selector-class-pattern': [
-            `^_?(${ALLOWED_PREFIXES.join('|')})`,
-            {
-                resolveNestedSelectors: true
-            }
-        ],
+        'selector-class-pattern': makeSelectorClassPatternOptions(),
 
         // SCSS rules
         'scss/operator-no-newline-after': null,
         'scss/selector-no-union-class-name': true,
-        'scss/at-mixin-pattern': [KEBAB_CASE_PATTERN, { message: 'Expected @mixin name to be kebab-case.' }],
+        'scss/at-mixin-pattern': [kebabCasePattern, { message: 'Expected @mixin name to be kebab-case.' }],
         'scss/no-unused-private-members': true
-    }
+    },
+    overrides: [
+        ...componentsRules,
+        ...componentsDevRules,
+        ...componentsExamplesRules,
+        ...docsRules
+    ]
 };
 
 module.exports = config;
