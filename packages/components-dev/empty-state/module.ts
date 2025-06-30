8be0f59d435e645000952e505d6391c176ed2078
Nikita Guryev <nikgurv@gmail.com>
2025-06-30 16:36:04 +0300

    feat(docs,empty-state): upd example image (#DS-2879)

--- packages/components-dev/empty-state/module.ts
+++ packages/components-dev/empty-state/module.ts
@@ -5,10 +5,53 @@ import { KbqComponentColors } from '@koobiq/components/core';
 import { KbqEmptyStateModule } from '@koobiq/components/empty-state';
 import { KbqIconModule } from '@koobiq/components/icon';
 import { EmptyStateExamplesModule } from '../../docs-examples/components/empty-state';
+import { DevThemeToggle } from '../theme-toggle';
 
 @Component({
     standalone: true,
-    imports: [FormsModule, KbqEmptyStateModule, KbqButtonModule, KbqIconModule, EmptyStateExamplesModule],
+    imports: [EmptyStateExamplesModule],
+    selector: 'dev-examples',
+    template: `
+        <empty-state-actions-example />
+        <hr />
+        <empty-state-actions2-example />
+        <hr />
+        <empty-state-align-example />
+        <hr />
+        <empty-state-big-example />
+        <hr />
+        <empty-state-content-example />
+        <hr />
+        <empty-state-default-example />
+        <hr />
+        <empty-state-error-example />
+        <hr />
+        <empty-state-icon-example />
+        <hr />
+        <empty-state-size-example />
+        <hr />
+        <empty-state-text-only-example />
+        <hr />
+        <empty-state-title-example />
+    `,
+    host: {
+        class: 'layout-column'
+    },
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+class DevExamples {}
+
+@Component({
+    standalone: true,
+    imports: [
+        FormsModule,
+        KbqEmptyStateModule,
+        KbqButtonModule,
+        KbqIconModule,
+        EmptyStateExamplesModule,
+        DevExamples,
+        DevThemeToggle
+    ],
     selector: 'dev-app',
     templateUrl: 'template.html',
     styleUrls: ['./styles.scss'],
