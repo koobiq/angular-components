c2b7943b24f63247aafafd49184d604d7584bda0
Nikita Guryev <nikgurv@gmail.com>
2025-07-29 15:47:40 +0300

    chore(input): added example for password change form (#DS-3965) (#928)

--- packages/docs-examples/components/input/index.ts
+++ packages/docs-examples/components/input/index.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
+import { InputChangePasswordExample } from './input-change-password/input-change-password-example';
 import { InputNumberOverviewExample } from './input-number-overview/input-number-overview-example';
 import { InputOverviewExample } from './input-overview/input-overview-example';
 import { InputPasswordOverviewExample } from './input-password-overview/input-password-overview-example';
 
-export { InputNumberOverviewExample, InputOverviewExample, InputPasswordOverviewExample };
+export { InputChangePasswordExample, InputNumberOverviewExample, InputOverviewExample, InputPasswordOverviewExample };
 
 const EXAMPLES = [
     InputOverviewExample,
     InputNumberOverviewExample,
-    InputPasswordOverviewExample
+    InputPasswordOverviewExample,
+    InputChangePasswordExample
 ];
 
 @NgModule({
