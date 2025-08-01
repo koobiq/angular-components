75b7d3447d88e969bab1df5939f63c9d566ef615
Nikita Guryev <nikgurv@gmail.com>
2025-08-01 16:32:23 +0300

    chore: fixed examples

--- packages/docs-examples/components/username/index.ts
+++ packages/docs-examples/components/username/index.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { UsernameCustomExample } from './username-custom/username-custom-example';
 import { UsernameOverviewExample } from './username-overview/username-overview-example';
 import { UsernamePlaygroundExample } from './username-playground/username-playground-example';
 
-export { UsernameOverviewExample, UsernamePlaygroundExample };
+export { UsernameCustomExample, UsernameOverviewExample, UsernamePlaygroundExample };
 
 const EXAMPLES = [
+    UsernameCustomExample,
     UsernameOverviewExample,
     UsernamePlaygroundExample
 ];
