ada1ebe5efab6f9fb91c326c9fda9ddce46af722
Nikita Guryev <nikgurv@gmail.com>
2025-08-07 11:26:24 +0300

    chore: fixed screenshot for dark theme

--- packages/components-dev/e2e/components/file-upload/file-upload.playwright-spec.ts
+++ packages/components-dev/e2e/components/file-upload/file-upload.playwright-spec.ts
@@ -8,6 +8,7 @@ test.describe('KbqFileUploadModule', () => {
         const getComponent = (page: Page) => page.getByTestId('e2eFileUploadStateAndStyle');
         const getSingleFileUploadTable = (locator: Locator) => locator.getByTestId('e2eSingleFileUploadTable');
         const getMultipleFileUploadTable = (locator: Locator) => locator.getByTestId('e2eMultipleFileUploadTable');
+        const focusFileItem = (locator: Locator) => locator.locator('.dev-focused .kbq-list-selection')?.focus();
 
         test('KbqSingleFileUploadComponent states', async ({ page }) => {
             await goToPage(page);
@@ -45,6 +46,8 @@ test.describe('KbqFileUploadModule', () => {
             const locator = getComponent(page);
 
             const screenshotTarget = getMultipleFileUploadTable(locator);
+
+            await focusFileItem(screenshotTarget);
 
             await expect(screenshotTarget).toHaveScreenshot();
         });
