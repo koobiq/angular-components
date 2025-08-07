aa57bc949d0f753b0cfd765269f89aa13c2dd105
Nikita Guryev <nikgurv@gmail.com>
2025-08-07 18:57:17 +0300

    chore: basic layout

--- packages/components/clamped-text/clamped-text.ts
+++ packages/components/clamped-text/clamped-text.ts
@@ -1,20 +1,64 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, InjectionToken, input, signal } from '@angular/core';
+import { KbqButtonModule } from '@koobiq/components/button';
+import { KbqIcon } from '@koobiq/components/icon';
+import { KbqLinkModule } from '@koobiq/components/link';
+import { KbqClampedTextConfig } from './types';
 
 const baseClass = 'kbq-clamped-text';
+
+export const KBQ_CLAMPED_TEXT_CONFIGURATION = new InjectionToken<KbqClampedTextConfig>('KbqInputFileLabel');
 
 @Component({
     selector: 'kbq-clamped-text',
     standalone: true,
-    exportAs: 'kbqUsername',
-    imports: [],
+    exportAs: 'kbqClampedText',
+    imports: [
+        KbqIcon,
+        KbqButtonModule,
+        KbqLinkModule
+    ],
     template: `
-        <ng-content />
+        <span #text class="kbq-clamped-text__content" [class.kbq-clamped-text__content_hidden]="!innerExpanded()">
+            <ng-content />
+        </span>
+
+        <span
+            class="kbq-clamped-text__toggle"
+            kbq-link
+            pseudo
+            role="link"
+            (click)="toggleExpanded()"
+            (keydown.enter)="toggleExpanded()"
+            (keydown.space)="toggleExpanded()"
+        >
+            <ng-content select="clamped-text-toggle,[clamped-text-toggle]" />
+
+            @if (innerExpanded()) {
+                <i kbq-icon="kbq-chevron-up-s_16"></i>
+                {{ config.closeText }}
+            } @else {
+                <i kbq-icon="kbq-chevron-down-s_16"></i>
+                {{ config.openText }}
+            }
+        </span>
     `,
     styleUrls: ['./clamped-text.scss', './clamped-text-tokens.scss'],
     host: {
-        class: baseClass
+        class: baseClass,
+        '[attr.aria-expanded]': 'this.innerExpanded().toString()'
     },
-    encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class KbqClampedText {}
+export class KbqClampedText {
+    expanded = input(false);
+    innerExpanded = signal(this.expanded());
+
+    config: KbqClampedTextConfig = {
+        openText: 'Развернуть',
+        closeText: 'Свернуть'
+    };
+
+    toggleExpanded() {
+        this.innerExpanded.update((state) => !state);
+    }
+}
