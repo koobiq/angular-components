e612fa11071426a000f9d2c577124c88c0a784c5
Nikita Guryev <nikgurv@gmail.com>
2025-08-11 17:01:21 +0300

    chore(file-upload): added screenshot tests (#DS-4031) (#963)

---
+++ packages/components-dev/e2e/components/file-upload.playwright-spec.ts
@@ -0,0 +1,55 @@
+import { expect, Locator, Page, test } from '@playwright/test';
+
+const getThemeToggle = (page: Page) => page.getByTestId('e2eThemeToggle');
+
+test.describe('KbqFileUploadModule', () => {
+    test.describe('DevFileUploadStateAndStyle', () => {
+        const goToPage = (page: Page) => page.goto('/');
+        const getComponent = (page: Page) => page.getByTestId('e2eFileUploadStateAndStyle');
+        const getSingleFileUploadTable = (locator: Locator) => locator.getByTestId('e2eSingleFileUploadTable');
+        const getMultipleFileUploadTable = (locator: Locator) => locator.getByTestId('e2eMultipleFileUploadTable');
+        const focusFileItem = (locator: Locator) => locator.locator('.dev-focused .kbq-list-selection')?.focus();
+
+        test('KbqSingleFileUploadComponent states', async ({ page }) => {
+            await goToPage(page);
+            const locator = getComponent(page);
+
+            const screenshotTarget = getSingleFileUploadTable(locator);
+
+            await expect(screenshotTarget).toHaveScreenshot();
+        });
+
+        test(`KbqSingleFileUploadComponent states (dark theme)`, async ({ page }) => {
+            await goToPage(page);
+            await getThemeToggle(page).click();
+
+            const locator = getComponent(page);
+
+            const screenshotTarget = getSingleFileUploadTable(locator);
+
+            await expect(screenshotTarget).toHaveScreenshot();
+        });
+
+        test('KbqMultipleFileUploadComponent states', async ({ page }) => {
+            await goToPage(page);
+            const locator = getComponent(page);
+
+            const screenshotTarget = getMultipleFileUploadTable(locator);
+
+            await expect(screenshotTarget).toHaveScreenshot();
+        });
+
+        test(`KbqMultipleFileUploadComponent states (dark theme)`, async ({ page }) => {
+            await goToPage(page);
+            await getThemeToggle(page).click();
+
+            const locator = getComponent(page);
+
+            const screenshotTarget = getMultipleFileUploadTable(locator);
+
+            await focusFileItem(screenshotTarget);
+
+            await expect(screenshotTarget).toHaveScreenshot();
+        });
+    });
+});
--- packages/components-dev/e2e/module.ts
+++ packages/components-dev/e2e/module.ts
@@ -2,12 +2,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { DevThemeToggle } from '../theme-toggle';
 import { DevActionsPanelWithOverlayContainer } from './components/actions-panel';
 import { DevButtonStateAndStyle } from './components/button';
+import { DevFileUploadStateAndStyle } from './components/file-upload';
 
 @Component({
     standalone: true,
     imports: [
         DevThemeToggle,
         DevButtonStateAndStyle,
+        DevFileUploadStateAndStyle,
         DevActionsPanelWithOverlayContainer
     ],
     selector: 'dev-app',
