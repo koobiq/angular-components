f29479802c3834c6b889accad1ec1b174632b9af
Artem Belik <artyombelyk@gmail.com>
2025-08-11 16:16:22 +0300

    chore(dev): updated top-bar dev application (#971)

--- packages/components-dev/top-bar/main.ts
+++ packages/components-dev/top-bar/main.ts
@@ -1,10 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { KBQ_LOCALE_SERVICE, KbqLocaleService } from '@koobiq/components/core';
 import { DevApp } from './module';
 
 bootstrapApplication(DevApp, {
     providers: [
         provideAnimations(),
+        { provide: KBQ_LOCALE_SERVICE, useClass: KbqLocaleService },
         provideRouter([])]
 }).catch((error) => console.error(error));
--- packages/components-dev/top-bar/module.ts
+++ packages/components-dev/top-bar/module.ts
@@ -2,6 +2,24 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
 import { KbqLocaleServiceModule } from '@koobiq/components/core';
 import { TopBarExamplesModule } from '../../docs-examples/components/top-bar';
 import { DevLocaleSelector } from '../locale-selector';
+import { DevThemeToggle } from '../theme-toggle';
+
+@Component({
+    standalone: true,
+    imports: [TopBarExamplesModule],
+    selector: 'dev-examples',
+    template: `
+        <top-bar-actions-example />
+        <!-- <top-bar-breadcrumbs-example /> -->
+        <!-- <top-bar-overflow-example /> -->
+        <!-- <top-bar-overview-example /> -->
+        <!-- <top-bar-title-counter-example /> -->
+        <!-- <top-bar-title-counter-adaptive-example /> -->
+        <!-- <top-bar-breadcrumbs-adaptive-example /> -->
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DevExamples {}
 
 @Component({
     standalone: true,
@@ -10,12 +28,11 @@ import { DevLocaleSelector } from '../locale-selector';
     encapsulation: ViewEncapsulation.None,
     templateUrl: './template.html',
     imports: [
-        TopBarExamplesModule,
         DevLocaleSelector,
-        KbqLocaleServiceModule
+        KbqLocaleServiceModule,
+        DevThemeToggle,
+        DevExamples
     ],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DevApp {
-    navbarUnpinned = false;
-}
+export class DevApp {}
