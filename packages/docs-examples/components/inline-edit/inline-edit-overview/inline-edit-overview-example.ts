c5cb822775006718148f5854bd29c78a11889bcb
Nikita Guryev <nikgurv@gmail.com>
2025-08-14 15:57:23 +0300

    chore: add edit/view toggle, edit mode in overlay

--- packages/docs-examples/components/inline-edit/inline-edit-overview/inline-edit-overview-example.ts
+++ packages/docs-examples/components/inline-edit/inline-edit-overview/inline-edit-overview-example.ts
@@ -1,16 +1,55 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { KbqInlineEdit } from '@koobiq/components/inline-edit';
+import { FormsModule } from '@angular/forms';
+import { KbqCleaner, KbqFormFieldModule, KbqLabel } from '@koobiq/components/form-field';
+import { KbqInlineEditModule } from '@koobiq/components/inline-edit';
+import { KbqInputModule } from '@koobiq/components/input';
+import { KbqSelectModule } from '@koobiq/components/select';
+import { KbqTagsModule } from '@koobiq/components/tags';
 
 /**
  * @title Inline edit overview
  */
 @Component({
     standalone: true,
-    imports: [KbqInlineEdit],
+    imports: [
+        KbqInlineEditModule,
+        KbqTagsModule,
+        KbqFormFieldModule,
+        KbqLabel,
+        KbqSelectModule,
+        KbqCleaner,
+        FormsModule,
+        KbqInputModule
+    ],
     selector: 'inline-edit-overview-example',
     template: `
-        <kbq-inline-edit />
+        <kbq-inline-edit>
+            <kbq-label>Field Name</kbq-label>
+
+            <ng-container *kbqInlineEditViewMode>
+                <kbq-tag-list>
+                    @for (tag of selected; track tag) {
+                        <kbq-tag [value]="tag">
+                            {{ tag }}
+                        </kbq-tag>
+                    }
+                </kbq-tag-list>
+            </ng-container>
+            <ng-container *kbqInlineEditEditMode>
+                <kbq-form-field>
+                    <kbq-select multiple [(value)]="selected">
+                        @for (option of options; track option) {
+                            <kbq-option [value]="option">{{ option }}</kbq-option>
+                        }
+                        <kbq-cleaner #kbqSelectCleaner />
+                    </kbq-select>
+                </kbq-form-field>
+            </ng-container>
+        </kbq-inline-edit>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class InlineEditOverviewExample {}
+export class InlineEditOverviewExample {
+    readonly options = Array.from({ length: 5 }).map((_, i) => `Option #${i}`);
+    selected: string[] = [];
+}
