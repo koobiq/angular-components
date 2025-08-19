b9931347b0e79a99507b5e6647d683435dbd553d
Artem Belik <artyombelyk@gmail.com>
2025-08-19 13:05:52 +0300

    chore(dev): updated markdown application (#DS-4073) (#980)

--- packages/components-dev/markdown/module.ts
+++ packages/components-dev/markdown/module.ts
@@ -1,113 +1,53 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { KbqMarkdownModule, KbqMarkdownService } from 'packages/components/markdown';
+import { MarkdownExamplesModule } from 'packages/docs-examples/components/markdown';
+import { DevThemeToggle } from '../theme-toggle';
 
 @Component({
     standalone: true,
-    imports: [KbqMarkdownModule],
-    selector: 'dev-inline-template-markdown',
-    // prettier-ignore
+    imports: [MarkdownExamplesModule],
+    selector: 'dev-examples',
     template: `
-<kbq-markdown ngPreserveWhitespaces>
-# H1
-
-## H2
-
-### H3
-
-#### H4
-
-##### H5
-
-###### H6
-
-**bold text**
-
-_italic text_
-
-> blockquote
-
-1. First item
-2. Second item
-3. Third item
-
--   First item
--   Second item
--   Third item
-
-\`inline code\`
-
----
-
-[title](https://www.koobiq.io)
-
-![](https://koobiq.io/assets/images/koobiq-illustration-wip.png)
-
-![With caption text](https://koobiq.io/assets/images/koobiq-illustration-wip.png)
-_Image caption text_
-
-| Syntax    | Description | Left-aligned | Center-aligned | Right-aligned |
-| --------- | ----------- | :----------- | :------------: | ------------: |
-| Header    | Title       | git status   |   git status   |    git status |
-| Paragraph | Text        | git diff     |    git diff    |      git diff |
-
-\`\`\`bash
-npm install jquery
-\`\`\`
-</kbq-markdown>
+        <markdown-article-example />
+        <hr />
+        <markdown-code-block-example />
+        <hr />
+        <markdown-code-example />
+        <hr />
+        <markdown-divider-example />
+        <hr />
+        <markdown-headers-combinations-example />
+        <hr />
+        <markdown-headers-example />
+        <hr />
+        <markdown-image-example />
+        <hr />
+        <markdown-line-break-example />
+        <hr />
+        <markdown-line-break-with-markdown-text-input-example />
+        <hr />
+        <markdown-link-example />
+        <hr />
+        <markdown-list-example />
+        <hr />
+        <markdown-paragraph-example />
+        <hr />
+        <markdown-quote-example />
+        <hr />
+        <markdown-selection-example />
+        <hr />
+        <markdown-table-example />
     `,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DevInlineTemplateMarkdown {}
+export class DevExamples {}
 
 @Component({
     standalone: true,
-    imports: [KbqMarkdownModule, DevInlineTemplateMarkdown],
+    imports: [DevExamples, DevThemeToggle],
     selector: 'dev-app',
     templateUrl: './template.html',
     styleUrl: './styles.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None
 })
-export class DevApp {
-    markdownText: string = `
-# Foobar
-
-Foobar is a Python library for dealing with word pluralization.
-
-## Installation
-
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
-
-\`\`\`bash
-pip install foobar
-\`\`\`
-
-## Usage
-
-\`\`\`python
-import foobar
-
-# returns 'words'
-foobar.pluralize('word')
-
-# returns 'geese'
-foobar.pluralize('goose')
-
-# returns 'phenomenon'
-foobar.singularize('phenomena')
-\`\`\`
-
-## Contributing
-Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
-
-Please make sure to update tests as appropriate.
-
-## License
-[MIT](https://choosealicense.com/licenses/mit/)
-`;
-    parsedByService: string;
-
-    constructor(readonly markdownService: KbqMarkdownService) {
-        this.parsedByService = markdownService.parseToHtml(this.markdownText);
-    }
-}
+export class DevApp {}
