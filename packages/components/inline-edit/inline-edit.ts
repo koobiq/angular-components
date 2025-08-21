90d028d1053a06af6e81e1e9f263b2f028da3c01
Nikita Guryev <nikgurv@gmail.com>
2025-08-21 19:39:15 +0300

    chore: added action buttons , cancel/save, fixed sizes of inline edit

--- packages/components/inline-edit/inline-edit.ts
+++ packages/components/inline-edit/inline-edit.ts
@@ -1,6 +1,9 @@
+import { animate, style, transition, trigger } from '@angular/animations';
+import { CdkTrapFocus } from '@angular/cdk/a11y';
 import { CdkConnectedOverlay, CdkOverlayOrigin } from '@angular/cdk/overlay';
 import { NgTemplateOutlet } from '@angular/common';
 import {
+    booleanAttribute,
     ChangeDetectionStrategy,
     Component,
     computed,
@@ -14,10 +17,22 @@ import {
     viewChild,
     ViewEncapsulation
 } from '@angular/core';
+import { AbstractControl, NgControl } from '@angular/forms';
 import { KbqButtonModule } from '@koobiq/components/button';
+import { KbqAnimationCurves, KbqAnimationDurations } from '@koobiq/components/core';
 import { KbqFormField, KbqLabel } from '@koobiq/components/form-field';
 import { KbqIcon } from '@koobiq/components/icon';
 import { KbqFocusMonitor } from './focus-monitor';
+
+const KBQ_INLINE_EDIT_ACTION_BUTTONS_ANIMATION = trigger('panelAnimation', [
+    transition(':enter', [
+        style({ transform: 'translateY(100%)', opacity: 0 }),
+        animate(
+            `${KbqAnimationDurations.Instant} ${KbqAnimationCurves.DecelerationCurve}`,
+            style({ transform: 'translateY(0%)', opacity: 1 })
+        )])
+
+]);
 
 const baseClass = 'kbq-inline-edit';
 
@@ -50,23 +65,26 @@ export class KbqInlineEditEditMode {
         '[class.kbq-inline-edit_with-label]': '!!label()',
         '[tabindex]': 'tabIndex()',
         '[class]': 'className()',
-        '(click)': 'onClick()',
-        '(keydown.enter)': 'onClick()',
-        '(keydown.space)': 'onClick()'
+        '(click)': 'onClick($event)',
+        '(keydown.enter)': 'onClick($event)',
+        '(keydown.space)': 'onClick($event)'
     },
     hostDirectives: [KbqFocusMonitor],
     imports: [
         NgTemplateOutlet,
         CdkConnectedOverlay,
         CdkOverlayOrigin,
+        CdkTrapFocus,
         KbqButtonModule,
         KbqIcon
     ],
+    animations: [KBQ_INLINE_EDIT_ACTION_BUTTONS_ANIMATION],
     changeDetection: ChangeDetectionStrategy.OnPush,
     encapsulation: ViewEncapsulation.None
 })
 export class KbqInlineEdit {
     readonly placeholder = input<string>();
+    readonly showActions = input(false, { transform: booleanAttribute });
 
     protected readonly viewModeTemplateRef = contentChild.required(KbqInlineEditViewMode);
     protected readonly editModeTemplateRef = contentChild.required(KbqInlineEditEditMode);
@@ -84,25 +102,108 @@ export class KbqInlineEdit {
             ? this.overlayOrigin()?.elementRef
             : this.elementRef;
 
-        return elementRef?.nativeElement.clientWidth ?? '';
+        return elementRef?.nativeElement.offsetWidth ?? '';
     });
 
     protected readonly elementRef = inject(ElementRef);
 
+    private initialValue: unknown;
+
+    /** @docs-private */
     toggleMode(): void {
         this.mode.update((mode) => (mode === 'view' ? 'edit' : 'view'));
     }
 
-    onClick(): void {
+    /** @docs-private */
+    onClick(event: Event): void {
+        event.stopPropagation();
         this.toggleMode();
     }
 
+    /** @docs-private */
     onAttach() {
         const formFieldRef = this.formFieldRef();
 
-        if (formFieldRef) {
-            formFieldRef.focus();
-            (formFieldRef.control as any).placeholder = this.placeholder();
+        setTimeout(() => {
+            if (formFieldRef) {
+                formFieldRef.focus();
+                (formFieldRef.control as any).placeholder = this.placeholder();
+
+                this.initialValue = this.getValue();
+            }
+        });
+    }
+
+    /** @docs-private */
+    onOutsideClick() {
+        this.save();
+        this.toggleMode();
+    }
+
+    /** @docs-private */
+    save() {
+        this.validate();
+    }
+
+    /** @docs-private */
+    cancel() {
+        this.setValue(this.initialValue);
+
+        this.toggleMode();
+    }
+
+    /** @docs-private */
+    onKeydown(event: KeyboardEvent) {
+        const { target, key } = event;
+
+        switch (key) {
+            case 'Escape': {
+                this.cancel();
+                break;
+            }
+            case 'Enter': {
+                if (target instanceof HTMLTextAreaElement) break;
+
+                this.onOutsideClick();
+                break;
+            }
+            default: {
+                return;
+            }
         }
+    }
+
+    private getValue() {
+        const control = this.coerceControl();
+
+        return control?.value;
+    }
+
+    private setValue<T>(value: T): void {
+        const control = this.coerceControl();
+
+        if (!control) return;
+
+        if (control instanceof AbstractControl) {
+            control.setValue(value);
+        } else {
+            control.value = value;
+        }
+    }
+
+    private coerceControl() {
+        const formFieldRef = this.formFieldRef();
+
+        if (!formFieldRef) return;
+
+        if (formFieldRef.control.ngControl instanceof NgControl) {
+            return formFieldRef.control.ngControl.control;
+        }
+
+        return formFieldRef.control;
+    }
+
+    private validate(): void {
+        console.log('validate');
     }
 }
