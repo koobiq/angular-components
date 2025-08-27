3feca4f2a3987ff34e6137ac0ef8eeca1f943348
lskramarov <l.s.kramarov@gmail.com>
2025-08-27 21:32:27 +0300

    fix: fixes after review

--- packages/components/navbar-ic/navbar-ic.animation.ts
+++ packages/components/navbar-ic/navbar-ic.animation.ts
@@ -2,7 +2,7 @@ import { animate, AnimationTriggerMetadata, state, style, transition, trigger } 
 
 export function toggleNavbarIcAnimation(): AnimationTriggerMetadata {
     return trigger('toggle', [
-        state('0', style({ width: 64 })),
-        state('1', style({ width: 240 })),
+        state('0', style({ width: '64px' })),
+        state('1', style({ width: '240px' })),
         transition('0 <=> 1', animate('150ms ease-out'))]);
 }
