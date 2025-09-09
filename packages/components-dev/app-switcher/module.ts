9b42599dd037dc42b0012680bc6723f754fb78e2
lskramarov <l.s.kramarov@gmail.com>
2025-09-09 16:15:15 +0300

    feat: added tree + layout + styles

--- packages/components-dev/app-switcher/module.ts
+++ packages/components-dev/app-switcher/module.ts
@@ -1,9 +1,16 @@
 import { A11yModule } from '@angular/cdk/a11y';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { KbqAppSwitcherModule } from '@koobiq/components/app-switcher';
+import {
+    buildFileTree,
+    DATA_OBJECT,
+    FileFlatNode, FileNode,
+    KbqAppSwitcherModule
+} from '@koobiq/components/app-switcher';
 import { KbqButtonModule } from '@koobiq/components/button';
-import { KbqIcon } from '@koobiq/components/icon';
+import { KbqComponentColors } from '@koobiq/components/core';
+import { KbqIcon, KbqIconButton } from '@koobiq/components/icon';
+import { FlatTreeControl, KbqTreeFlatDataSource, KbqTreeFlattener, KbqTreeModule } from '@koobiq/components/tree';
 import { PopoverExamplesModule } from 'packages/docs-examples/components/popover';
 
 @Component({
@@ -27,10 +34,72 @@ class DevExamples {}
     imports: [
         A11yModule,
         FormsModule,
+        DevExamples,
         KbqAppSwitcherModule,
-        DevExamples
+        KbqTreeModule,
+        KbqIconButton
     ],
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DevApp {}
+export class DevApp {
+    componentColors = KbqComponentColors;
+
+    treeControl: FlatTreeControl<FileFlatNode>;
+    treeFlattener: KbqTreeFlattener<FileNode, FileFlatNode>;
+    dataSource: KbqTreeFlatDataSource<FileNode, FileFlatNode>;
+
+    modelValue: any = '';
+
+    constructor() {
+        this.treeFlattener = new KbqTreeFlattener(this.transformer, this.getLevel, this.isExpandable, this.getChildren);
+
+        this.treeControl = new FlatTreeControl<FileFlatNode>(
+            this.getLevel,
+            this.isExpandable,
+            this.getValue,
+            this.getViewValue
+        );
+        this.dataSource = new KbqTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+        this.dataSource.data = buildFileTree(DATA_OBJECT, 0);
+    }
+
+    hasChild(_: number, nodeData: FileFlatNode) {
+        return nodeData.expandable;
+    }
+
+    private transformer = (node: FileNode, level: number, parent: any) => {
+        const flatNode = new FileFlatNode();
+
+        flatNode.name = node.name;
+        flatNode.parent = parent;
+        flatNode.type = node.type;
+        flatNode.level = level;
+        flatNode.expandable = !!node.children;
+
+        return flatNode;
+    };
+
+    private getLevel = (node: FileFlatNode) => {
+        return node.level;
+    };
+
+    private isExpandable = (node: FileFlatNode) => {
+        return node.expandable;
+    };
+
+    private getChildren = (node: FileNode): FileNode[] => {
+        return node.children;
+    };
+
+    private getValue = (node: FileFlatNode): string => {
+        return node.name;
+    };
+
+    private getViewValue = (node: FileFlatNode): string => {
+        const nodeType = node.type ? `.${node.type}` : '';
+
+        return `${node.name}${nodeType}`;
+    };
+}
--- packages/components/app-switcher/app-switcher-tree.ts
+++ packages/components/app-switcher/app-switcher-tree.ts
@@ -1,17 +1,43 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { kbqAppSwitcherAnimations } from './app-switcher-animations';
+import {
+    ChangeDetectionStrategy,
+    Component,
+    forwardRef,
+    ViewEncapsulation
+} from '@angular/core';
+import { FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { KbqHighlightModule } from '@koobiq/components/core';
+import {
+    KBQ_TREE_OPTION_PARENT_COMPONENT,
+    KbqTreeBase,
+    KbqTreeModule,
+    KbqTreeSelection
+} from '@koobiq/components/tree';
 
 @Component({
     standalone: true,
     selector: 'kbq-app-switcher-tree',
-    templateUrl: './app-switcher-tree.html',
+    template: '<ng-container kbqTreeNodeOutlet />',
     preserveWhitespaces: false,
     styleUrls: ['./app-switcher-tree.scss'],
     host: {
-        class: 'kbq-app-switcher'
+        class: 'kbq-app-switcher-tree',
+        '[class.kbq-tree-selection]': 'null'
     },
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
-    animations: [kbqAppSwitcherAnimations.state]
+    imports: [
+        KbqHighlightModule,
+        KbqTreeModule,
+        FormsModule
+    ],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => KbqAppSwitcherTree),
+            multi: true
+        },
+        { provide: KBQ_TREE_OPTION_PARENT_COMPONENT, useExisting: KbqAppSwitcherTree },
+        { provide: KbqTreeBase, useExisting: KbqAppSwitcherTree }
+    ]
 })
-export class KbqAppSwitcherTree {}
+export class KbqAppSwitcherTree extends KbqTreeSelection {}
