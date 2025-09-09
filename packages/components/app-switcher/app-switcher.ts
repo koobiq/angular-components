6f229174be58f81d49f4a30a9487c93d011fc345
lskramarov <l.s.kramarov@gmail.com>
2025-09-09 16:15:15 +0300

    feat: added tree + layout + styles

--- packages/components/app-switcher/app-switcher.ts
+++ packages/components/app-switcher/app-switcher.ts
@@ -29,6 +29,7 @@ import {
 } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
+import { KbqBadgeModule } from '@koobiq/components/badge';
 import {
     KbqComponentColors,
     KbqPopUp,
@@ -40,11 +41,102 @@ import {
 } from '@koobiq/components/core';
 import { KbqDividerModule } from '@koobiq/components/divider';
 import { KbqFormFieldModule } from '@koobiq/components/form-field';
+import { KbqIcon } from '@koobiq/components/icon';
 import { KbqInputModule } from '@koobiq/components/input';
 import { defaultOffsetYWithArrow } from '@koobiq/components/popover';
+import { FlatTreeControl, KbqTreeFlatDataSource, KbqTreeFlattener, KbqTreeModule } from '@koobiq/components/tree';
 import { merge } from 'rxjs';
 import { kbqAppSwitcherAnimations } from './app-switcher-animations';
 import { KbqAppSwitcherTree } from './app-switcher-tree';
+import { KbqAppSwitcherTreeNodePadding, KbqAppSwitcherTreeOption } from './app-switcher-tree-option';
+
+export class FileNode {
+    children: FileNode[];
+    name: string;
+    type: any;
+}
+
+/** Flat node with expandable and level information */
+export class FileFlatNode {
+    name: string;
+    type: any;
+    level: number;
+    expandable: boolean;
+    parent: any;
+}
+
+export function buildFileTree(value: any, level: number): FileNode[] {
+    const data: any[] = [];
+
+    for (const k of Object.keys(value)) {
+        const v = value[k];
+        const node = new FileNode();
+
+        node.name = `${k}`;
+
+        if (v === null || v === undefined) {
+            // no action
+        } else if (typeof v === 'object') {
+            node.children = buildFileTree(v, level + 1);
+        } else {
+            node.type = v;
+        }
+
+        data.push(node);
+    }
+
+    return data;
+}
+
+export const DATA_OBJECT = {
+    docs: 'app',
+    src: {
+        cdk: {
+            a11ly: {
+                'aria describer': {
+                    'aria-describer': 'ts',
+                    'aria-describer.spec': 'ts',
+                    'aria-reference': 'ts',
+                    'aria-reference.spec': 'ts'
+                },
+                'focus monitor': {
+                    'focus-monitor': 'ts',
+                    'focus-monitor.spec': 'ts'
+                }
+            }
+        },
+        documentation: {
+            source: '',
+            tools: ''
+        },
+        mosaic: {
+            autocomplete: '',
+            button: '',
+            'button-toggle': '',
+            index: 'ts',
+            package: 'json',
+            version: 'ts'
+        },
+        'components-dev': {
+            alert: '',
+            badge: ''
+        },
+        'docs-examples': '',
+        'angular-moment-adapter': '',
+        README: 'md',
+        'tsconfig.build': 'json'
+    },
+    scripts: {
+        deploy: {
+            'cleanup-preview': 'ts',
+            'publish-artifacts': 'sh',
+            'publish-docs': 'sh',
+            'publish-docs-preview': 'ts'
+        },
+        'tsconfig.deploy': 'json'
+    },
+    tests: ''
+};
 
 @Component({
     standalone: true,
@@ -62,7 +154,12 @@ import { KbqAppSwitcherTree } from './app-switcher-tree';
         KbqInputModule,
         FormsModule,
         KbqDividerModule,
-        KbqAppSwitcherTree
+        KbqAppSwitcherTree,
+        KbqBadgeModule,
+        KbqIcon,
+        KbqTreeModule,
+        KbqAppSwitcherTreeOption,
+        KbqAppSwitcherTreeNodePadding
     ],
     animations: [kbqAppSwitcherAnimations.state]
 })
@@ -82,6 +179,22 @@ export class KbqAppSwitcher extends KbqPopUp implements AfterViewInit {
     @ViewChild('appSwitcherContent') appSwitcherContent: ElementRef<HTMLDivElement>;
     @ViewChild('appSwitcher') elementRef: ElementRef;
     @ViewChild(CdkTrapFocus) cdkTrapFocus: CdkTrapFocus;
+
+    constructor() {
+        super();
+
+        this.treeFlattener = new KbqTreeFlattener(this.transformer, this.getLevel, this.isExpandable, this.getChildren);
+
+        this.treeControl = new FlatTreeControl<FileFlatNode>(
+            this.getLevel,
+            this.isExpandable,
+            this.getValue,
+            this.getViewValue
+        );
+        this.dataSource = new KbqTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+        this.dataSource.data = buildFileTree(DATA_OBJECT, 0);
+    }
 
     ngAfterViewInit() {
         if (!this.appSwitcherContent) {
@@ -108,6 +221,51 @@ export class KbqAppSwitcher extends KbqPopUp implements AfterViewInit {
     updateTrapFocus(isTrapFocus: boolean): void {
         this.isTrapFocus = isTrapFocus;
     }
+
+    treeControl: FlatTreeControl<FileFlatNode>;
+    treeFlattener: KbqTreeFlattener<FileNode, FileFlatNode>;
+
+    dataSource: KbqTreeFlatDataSource<FileNode, FileFlatNode>;
+
+    modelValue: any = '';
+
+    hasChild(_: number, nodeData: FileFlatNode) {
+        return nodeData.expandable;
+    }
+
+    private transformer = (node: FileNode, level: number, parent: any) => {
+        const flatNode = new FileFlatNode();
+
+        flatNode.name = node.name;
+        flatNode.parent = parent;
+        flatNode.type = node.type;
+        flatNode.level = level;
+        flatNode.expandable = !!node.children;
+
+        return flatNode;
+    };
+
+    private getLevel = (node: FileFlatNode) => {
+        return node.level;
+    };
+
+    private isExpandable = (node: FileFlatNode) => {
+        return node.expandable;
+    };
+
+    private getChildren = (node: FileNode): FileNode[] => {
+        return node.children;
+    };
+
+    private getValue = (node: FileFlatNode): string => {
+        return node.name;
+    };
+
+    private getViewValue = (node: FileFlatNode): string => {
+        const nodeType = node.type ? `.${node.type}` : '';
+
+        return `${node.name}${nodeType}`;
+    };
 }
 
 export const KBQ_APP_SWITCHER_SCROLL_STRATEGY = new InjectionToken<() => ScrollStrategy>(
@@ -127,6 +285,7 @@ export const KBQ_APP_SWITCHER_SCROLL_STRATEGY_FACTORY_PROVIDER = {
 };
 
 @Directive({
+    standalone: true,
     selector: '[kbqAppSwitcher]',
     exportAs: 'kbqAppSwitcher',
     host: {
