1ccf345de0d1500a5bf4b3d98d39b4aadc02ccb0
lskramarov <l.s.kramarov@gmail.com>
2025-09-17 17:02:05 +0300

    feat: kbq-app-switcher-dropdown-app & kbq-app-switcher-dropdown-site

--- packages/components/app-switcher/public-api.ts
+++ packages/components/app-switcher/public-api.ts
@@ -1,4 +1,7 @@
 export * from './app-switcher';
 export * from './app-switcher-animations';
+export * from './app-switcher-dropdown-app';
+export * from './app-switcher-dropdown-site';
 export * from './app-switcher-tree';
+export * from './app-switcher-tree-option';
 export * from './app-switcher.module';
