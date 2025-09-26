2606f8353f7571941d34719fa5f00d1e720adf36
lskramarov <l.s.kramarov@gmail.com>
2025-09-26 15:55:45 +0300

    fix: styles and params

--- packages/components-dev/app-switcher/module.ts
+++ packages/components-dev/app-switcher/module.ts
@@ -1,22 +1,13 @@
 import { A11yModule } from '@angular/cdk/a11y';
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {
-    buildFileTree,
-    DATA_OBJECT,
-    FileFlatNode,
-    FileNode,
-    KbqAppSwitcherApp,
-    KbqAppSwitcherModule
-} from '@koobiq/components/app-switcher';
+import { KbqAppSwitcherModule } from '@koobiq/components/app-switcher';
 import { KbqBadgeModule } from '@koobiq/components/badge';
 import { KbqButtonModule } from '@koobiq/components/button';
 import { KbqComponentColors, KbqOptionModule } from '@koobiq/components/core';
 import { KbqDividerModule } from '@koobiq/components/divider';
 import { KbqDropdownModule } from '@koobiq/components/dropdown';
 import { KbqIcon } from '@koobiq/components/icon';
-import { KbqScrollbar } from '@koobiq/components/scrollbar';
-import { FlatTreeControl, KbqTreeFlatDataSource, KbqTreeFlattener, KbqTreeModule } from '@koobiq/components/tree';
 import { PopoverExamplesModule } from 'packages/docs-examples/components/popover';
 
 @Component({
@@ -24,44 +15,13 @@ import { PopoverExamplesModule } from 'packages/docs-examples/components/popover
     imports: [PopoverExamplesModule, KbqButtonModule, KbqIcon, KbqAppSwitcherModule],
     selector: 'dev-examples',
     template: `
-        <button kbq-button kbqAppSwitcher>
+        <button kbq-button kbqAppSwitcher [search]="true" [sites]="sites">
             <i kbq-icon="kbq-bento-menu_16"></i>
         </button>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-class DevExamples {}
-
-@Component({
-    standalone: true,
-    selector: 'dev-app',
-    styleUrls: ['./styles.scss'],
-    templateUrl: './template.html',
-    imports: [
-        A11yModule,
-        FormsModule,
-        DevExamples,
-        KbqAppSwitcherModule,
-        KbqTreeModule,
-        KbqDropdownModule,
-        KbqDividerModule,
-        KbqBadgeModule,
-        KbqScrollbar,
-        KbqAppSwitcherApp,
-        KbqOptionModule
-    ],
-    encapsulation: ViewEncapsulation.None,
-    changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class DevApp {
-    componentColors = KbqComponentColors;
-
-    treeControl: FlatTreeControl<FileFlatNode>;
-    treeFlattener: KbqTreeFlattener<FileNode, FileFlatNode>;
-    dataSource: KbqTreeFlatDataSource<FileNode, FileFlatNode>;
-
-    modelValue: any = '';
-
+class DevExamples {
     sites = [
         {
             name: 'ЦФО',
@@ -364,59 +324,31 @@ export class DevApp {
             ]
         }
     ];
+}
+
+@Component({
+    standalone: true,
+    selector: 'dev-app',
+    styleUrls: ['./styles.scss'],
+    templateUrl: './template.html',
+    imports: [
+        A11yModule,
+        FormsModule,
+        DevExamples,
+        KbqAppSwitcherModule,
+        KbqDropdownModule,
+        KbqDividerModule,
+        KbqBadgeModule,
+        KbqOptionModule
+    ],
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DevApp {
+    componentColors = KbqComponentColors;
+
+    modelValue: any = '';
 
     activeSite;
     activeApp;
-
-    constructor() {
-        this.treeFlattener = new KbqTreeFlattener(this.transformer, this.getLevel, this.isExpandable, this.getChildren);
-
-        this.treeControl = new FlatTreeControl<FileFlatNode>(
-            this.getLevel,
-            this.isExpandable,
-            this.getValue,
-            this.getViewValue
-        );
-        this.dataSource = new KbqTreeFlatDataSource(this.treeControl, this.treeFlattener);
-
-        this.dataSource.data = buildFileTree(DATA_OBJECT, 0);
-    }
-
-    hasChild(_: number, nodeData: FileFlatNode) {
-        return nodeData.expandable;
-    }
-
-    private transformer = (node: FileNode, level: number, parent: any) => {
-        const flatNode = new FileFlatNode();
-
-        flatNode.name = node.name;
-        flatNode.parent = parent;
-        flatNode.type = node.type;
-        flatNode.level = level;
-        flatNode.expandable = !!node.children;
-
-        return flatNode;
-    };
-
-    private getLevel = (node: FileFlatNode) => {
-        return node.level;
-    };
-
-    private isExpandable = (node: FileFlatNode) => {
-        return node.expandable;
-    };
-
-    private getChildren = (node: FileNode): FileNode[] => {
-        return node.children;
-    };
-
-    private getValue = (node: FileFlatNode): string => {
-        return node.name;
-    };
-
-    private getViewValue = (node: FileFlatNode): string => {
-        const nodeType = node.type ? `.${node.type}` : '';
-
-        return `${node.name}${nodeType}`;
-    };
 }
