fa536f73e234f40dc6eac4e25a57c47caa1cb520
Nikita Guryev <nikgurv@gmail.com>
2025-11-19 18:29:18 +0300

    feat(schematics): init icon-replacement schematic (#DS-4363)

---
+++ packages/schematics/src/migrations/icons-replacement/README.md
@@ -0,0 +1,62 @@
+### Loader Overlay Size Attr schematics
+
+This schematic provides migration for `KbqLoaderOverlay` new `size` attribute. It includes:
+
+- Removal of `compact` attribute usage
+- Replacement with `size` attribute
+
+[Params](schema.ts)
+
+Usage for Angular Cli:
+
+```shell
+ng g @koobiq/components:loader-overlay-size-attr --project <your project>
+```
+
+Usage for Nx:
+
+```shell
+nx g @koobiq/components:loader-overlay-size-attr --project <your project>
+```
+
+### Run locally
+
+Build package
+
+```shell
+yarn run build:schematics
+```
+
+Run command (for example, for `koobiq-docs` project)
+
+```shell
+ng g ./dist/components/schematics/collection.json:loader-overlay-size-attr --project koobiq-docs
+```
+
+### Result
+
+#### Before
+
+```ts
+import { Component } from '@angular/core';
+
+@Component({
+    template: `
+        <kbq-loader-overlay [compact]="true">...</kbq-loader-overlay>
+    `
+})
+export class MyComp {}
+```
+
+#### After
+
+```ts
+import { Component } from '@angular/core';
+
+@Component({
+    template: `
+        <kbq-loader-overlay size="compact">...</kbq-loader-overlay>
+    `
+})
+export class MyComp {}
+```
---
+++ packages/schematics/src/migrations/icons-replacement/index.spec.ts
@@ -0,0 +1,81 @@
+import { ProjectDefinitionCollection } from '@angular-devkit/core/src/workspace';
+import { Tree } from '@angular-devkit/schematics';
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { ProjectDefinition, WorkspaceDefinition } from '@schematics/angular/utility';
+import { getWorkspace } from '@schematics/angular/utility/workspace';
+import * as path from 'path';
+import { createTestApp } from '../../utils/testing';
+import { Schema } from './schema';
+
+const collectionPath = path.join(__dirname, '../../collection.json');
+const SCHEMATIC_NAME = 'loader-overlay-size-attr';
+
+const getProjectContentPaths = (project: ProjectDefinition) => {
+    return {
+        templatePath: `/${project.root}/src/app/app.component.html`,
+        tsPath: `/${project.root}/src/app/app.component.ts`,
+        stylesPath: `/${project.root}/src/styles.scss`
+    };
+};
+
+describe(SCHEMATIC_NAME, () => {
+    let runner: SchematicTestRunner;
+    let appTree: Tree;
+    let projects: ProjectDefinitionCollection;
+    let workspace: WorkspaceDefinition;
+
+    beforeEach(async () => {
+        runner = new SchematicTestRunner('schematics', collectionPath);
+        appTree = await createTestApp(runner, { style: 'scss' });
+        workspace = await getWorkspace(appTree);
+        projects = workspace.projects as unknown as ProjectDefinitionCollection;
+    });
+
+    it('should run migration for specified project', async () => {
+        const [firstProjectKey] = projects.keys();
+
+        await runner.runSchematic(SCHEMATIC_NAME, { project: firstProjectKey } satisfies Schema, appTree);
+    });
+
+    it('should run migration for external html', async () => {
+        const [firstProjectKey] = projects.keys();
+        const { templatePath } = getProjectContentPaths(projects.get(firstProjectKey)!);
+
+        const template =
+            '<div>' +
+            '<kbq-loader-overlay [compact]="false"></kbq-loader-overlay>' +
+            '<kbq-loader-overlay [compact]="true"></kbq-loader-overlay>' +
+            '</div>';
+
+        appTree.overwrite(templatePath, template);
+
+        const updatedTree = await runner.runSchematic(
+            SCHEMATIC_NAME,
+            { project: firstProjectKey } satisfies Schema,
+            appTree
+        );
+
+        expect(updatedTree.read(templatePath)?.toString()).toMatchSnapshot(`project ${firstProjectKey}: after changes`);
+    });
+
+    it('should throw message if replaced attr value is not static', async () => {
+        const warnSpy = jest.spyOn(console, 'warn').mockImplementation();
+        const [firstProjectKey] = projects.keys();
+        const { templatePath } = getProjectContentPaths(projects.get(firstProjectKey)!);
+
+        // Set up the template with a non-static attribute value
+        const nonStaticTemplate = '<div><kbq-loader-overlay [compact]="VARIABLE"></kbq-loader-overlay></div>';
+
+        appTree.overwrite(templatePath, nonStaticTemplate);
+        const templateBeforeUpdate = appTree.read(templatePath)?.toString();
+
+        const updatedTree = await runner.runSchematic(
+            SCHEMATIC_NAME,
+            { project: firstProjectKey } satisfies Schema,
+            appTree
+        );
+
+        expect(templateBeforeUpdate).toBe(updatedTree.read(templatePath)?.toString());
+        expect(warnSpy.mock.calls.some(([msg]) => msg.includes(templatePath))).toBe(true);
+    });
+});
