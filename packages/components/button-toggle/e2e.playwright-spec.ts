725ccbbf6837a5387ad4532645566ae6d782a170
lskramarov <l.s.kramarov@gmail.com>
2025-12-08 16:13:52 +0300

    feat(button-toggle): added e2e tests (#DS-4398)

---
+++ packages/components/button-toggle/e2e.playwright-spec.ts
@@ -0,0 +1,51 @@
+import { expect, Locator, Page, test } from '@playwright/test';
+import { devEnableDarkTheme, devGoToRootPage } from '../../e2e/utils';
+
+test.describe('KbqButtonToggleModule', () => {
+    test.describe('E2eButtonToggleStates', () => {
+        const getComponent = (page: Page) => page.getByTestId('e2eButtonToggleStates');
+        const togglePrefix = (locator: Locator) => locator.getByTestId('e2eShowPrefixIcon').click();
+        const toggleTitle = (locator: Locator) => locator.getByTestId('e2eShowTitle').click();
+        const toggleSuffix = (locator: Locator) => locator.getByTestId('e2eShowSuffixIcon').click();
+        const getScreenshotTarget = (locator: Locator) => locator.getByTestId('e2eScreenshotTarget');
+
+        test('button-toggle with title', async ({ page }) => {
+            await devGoToRootPage(page);
+            const locator = getComponent(page);
+
+            await expect(getScreenshotTarget(locator)).toHaveScreenshot();
+        });
+
+        test('button-toggle with icon', async ({ page }) => {
+            await devGoToRootPage(page);
+            const locator = getComponent(page);
+
+            await togglePrefix(locator);
+            await toggleTitle(locator);
+
+            await expect(getScreenshotTarget(locator)).toHaveScreenshot();
+        });
+
+        test('button-toggle with title, prefix and suffix', async ({ page }) => {
+            await devGoToRootPage(page);
+            const locator = getComponent(page);
+
+            await togglePrefix(locator);
+            await toggleSuffix(locator);
+
+            await expect(getScreenshotTarget(locator)).toHaveScreenshot();
+        });
+
+        test('button-toggle with title, prefix and suffix (dark theme)', async ({ page }) => {
+            await devGoToRootPage(page);
+            await devEnableDarkTheme(page);
+
+            const locator = getComponent(page);
+
+            await togglePrefix(locator);
+            await toggleSuffix(locator);
+
+            await expect(getScreenshotTarget(locator)).toHaveScreenshot();
+        });
+    });
+});
---
+++ packages/components/button-toggle/e2e.ts
@@ -0,0 +1,131 @@
+import { ChangeDetectionStrategy, Component, model } from '@angular/core';
+import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
+import { KbqButtonModule } from '@koobiq/components/button';
+import { KbqCheckboxModule } from '@koobiq/components/checkbox';
+import { KbqIconModule } from '@koobiq/components/icon';
+import { combineLatest } from 'rxjs';
+import { KbqButtonToggle, KbqButtonToggleGroup } from './button-toggle.component';
+
+type E2eButtonState = Partial<{
+    title: string;
+    disabled: boolean;
+    checked: boolean;
+    hover: boolean;
+    active: boolean;
+    focused?: boolean;
+    progress: boolean;
+}>;
+
+type E2eButtonOrientation = Partial<{
+    vertical: boolean;
+    multiple: boolean;
+}>;
+
+type DevButton = E2eButtonState & E2eButtonOrientation;
+
+@Component({
+    selector: 'e2e-button-toggle-state-and-style',
+    imports: [KbqIconModule, FormsModule, KbqCheckboxModule, KbqButtonToggle, KbqButtonToggleGroup, KbqButtonModule],
+    template: `
+        <div class="dev-options">
+            <kbq-checkbox data-testid="e2eShowPrefixIcon" [(ngModel)]="showPrefixIcon">show prefix icon</kbq-checkbox>
+            <kbq-checkbox
+                data-testid="e2eShowTitle"
+                [disabled]="!showPrefixIcon() && !showSuffixIcon()"
+                [(ngModel)]="showTitle"
+            >
+                show title
+            </kbq-checkbox>
+            <kbq-checkbox data-testid="e2eShowSuffixIcon" [(ngModel)]="showSuffixIcon">show suffix icon</kbq-checkbox>
+        </div>
+
+        <table data-testid="e2eScreenshotTarget">
+            @for (buttons of rows; track buttons) {
+                <tr>
+                    @for (button of buttons; track button.title) {
+                        <td>
+                            <kbq-button-toggle-group [vertical]="button.vertical!" [multiple]="button.multiple!">
+                                <kbq-button-toggle
+                                    [value]="1"
+                                    [checked]="button.checked!"
+                                    [class.cdk-keyboard-focused]="button.focused"
+                                    [class.kbq-active]="button.active"
+                                    [class.kbq-hover]="button.hover"
+                                    [class.kbq-progress]="button.progress"
+                                    [disabled]="button.disabled!"
+                                >
+                                    @if (showPrefixIcon()) {
+                                        <i kbq-icon="kbq-play_16"></i>
+                                    }
+                                    @if (showTitle()) {
+                                        {{ button.title }}
+                                    }
+                                    @if (showSuffixIcon()) {
+                                        <i kbq-icon="kbq-chevron-down-s_16"></i>
+                                    }
+                                </kbq-button-toggle>
+                                <kbq-button-toggle [value]="2" [checked]="button.multiple! && button.checked!">
+                                    default 2
+                                </kbq-button-toggle>
+                                <kbq-button-toggle [value]="3">default 3</kbq-button-toggle>
+                            </kbq-button-toggle-group>
+                        </td>
+                    }
+                </tr>
+            }
+        </table>
+    `,
+    styles: `
+        .dev-options {
+            display: flex;
+            gap: var(--kbq-size-m);
+            margin-bottom: var(--kbq-size-l);
+            padding: var(--kbq-size-xxs);
+        }
+
+        table {
+            border-spacing: 0;
+        }
+
+        td {
+            padding: var(--kbq-size-xxs);
+        }
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    host: {
+        'data-testid': 'e2eButtonToggleStates'
+    }
+})
+export class E2eButtonToggleStates {
+    readonly showPrefixIcon = model(false);
+    readonly showTitle = model(true);
+    readonly showSuffixIcon = model(false);
+
+    private readonly states: E2eButtonState[] = [
+        { title: 'disabled', disabled: true },
+        { title: 'checked', checked: true },
+        { title: 'normal' },
+        { title: 'hover', hover: true },
+        { title: 'active', active: true },
+        { title: 'focus', focused: true },
+        { title: 'progress', progress: true }];
+
+    private readonly orientation: E2eButtonOrientation[] = [
+        { vertical: false },
+        { multiple: true },
+        { vertical: true },
+        { vertical: true, multiple: true }];
+
+    readonly rows: DevButton[][] = this.orientation.map((style) =>
+        this.states.map((state) => ({ ...state, ...style }))
+    );
+
+    constructor() {
+        combineLatest([toObservable(this.showPrefixIcon), toObservable(this.showSuffixIcon)])
+            .pipe(takeUntilDestroyed())
+            .subscribe((args) => {
+                if (args.every((a) => a === false)) this.showTitle.set(true);
+            });
+    }
+}
