95a64ba580ff6f053d186d7bc7bb39eccab57372
Nikita Guryev <nikgurv@gmail.com>
2025-12-08 15:10:29 +0300

    feat(clamped-text): added screenshot tests (#DS-4440)

--- packages/e2e/module.ts
+++ packages/e2e/module.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { DevThemeToggle } from '../components-dev/theme-toggle';
 import { E2eActionsPanelWithOverlayContainer } from '../components/actions-panel/e2e';
 import { E2eButtonStateAndStyle } from '../components/button/e2e';
+import { E2eClampedTextStateAndStyle } from '../components/clamped-text/e2e';
 import { E2eFileUploadStateAndStyle } from '../components/file-upload/e2e';
 import { E2eSplitButtonStateAndStyle } from '../components/split-button/e2e';
 import { E2eTagEditable, E2eTagStateAndStyle } from '../components/tags/e2e';
@@ -18,7 +19,8 @@ import { E2eToggleStateAndStyle, E2eToggleWithTextAndCaption } from '../componen
         E2eTagStateAndStyle,
         E2eTagEditable,
         E2eToggleStateAndStyle,
-        E2eToggleWithTextAndCaption
+        E2eToggleWithTextAndCaption,
+        E2eClampedTextStateAndStyle
     ],
     templateUrl: 'template.html',
     styleUrl: 'main.scss',
