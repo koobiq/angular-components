f0b0464c288b707dbe0a9a888fd1fa4af3f45299
Artem Belik <artyombelyk@gmail.com>
2025-12-17 09:31:11 +0300

    refactor: merge conflict

--- packages/components/badge/e2e.playwright-spec.ts
+++ packages/components/badge/e2e.playwright-spec.ts
@@ -1,18 +1,18 @@
 import { expect, Page, test } from '@playwright/test';
-import { devEnableDarkTheme, devGoToRootPage } from '../../e2e/utils';
+import { e2eEnableDarkTheme, e2eGoToRootPage } from 'packages/e2e/utils';
 
 test.describe('KbqBadgeModule', () => {
     test.describe('E2eBadgeStyles', () => {
         const getComponent = (page: Page) => page.getByTestId('e2eBadgeStyles');
 
         test('light theme', async ({ page }) => {
-            await devGoToRootPage(page);
+            await e2eGoToRootPage(page);
             await expect(getComponent(page)).toHaveScreenshot();
         });
 
         test('dark theme', async ({ page }) => {
-            await devGoToRootPage(page);
-            await devEnableDarkTheme(page);
+            await e2eGoToRootPage(page);
+            await e2eEnableDarkTheme(page);
             await expect(getComponent(page)).toHaveScreenshot();
         });
     });
