496e835c3118cab800cd3f0b71a0a709c025f761
Nikita Guryev <nikgurv@gmail.com>
2025-12-22 22:19:54 +0300

    chore(progress-bar): added e2e tests (#DS-4446)

---
+++ packages/components/progress-bar/e2e.playwright-spec.ts
@@ -0,0 +1,33 @@
+import { expect, Locator, Page, test } from '@playwright/test';
+import { e2eEnableDarkTheme, e2eGoToRootPage } from '../../e2e/utils';
+
+test.describe('KbqProgressBar', () => {
+    test.describe('E2eProgressBarStateAndStyle', () => {
+        const getComponent = (page: Page) => page.getByTestId('e2eProgressBarStateAndStyle');
+        const getTestTable = (locator: Locator) => locator.getByTestId('e2eProgressBarTable');
+
+        test('KbqProgressBar states', async ({ page }) => {
+            await e2eGoToRootPage(page);
+            const locator = getComponent(page);
+
+            const screenshotTarget = getTestTable(locator);
+
+            await page.waitForTimeout(50);
+
+            await expect(screenshotTarget).toHaveScreenshot();
+        });
+
+        test(`KbqProgressBar states (dark theme)`, async ({ page }) => {
+            await e2eGoToRootPage(page);
+            await e2eEnableDarkTheme(page);
+
+            const locator = getComponent(page);
+
+            const screenshotTarget = getTestTable(locator);
+
+            await page.waitForTimeout(50);
+
+            await expect(screenshotTarget).toHaveScreenshot();
+        });
+    });
+});
--- packages/e2e/module.ts
+++ packages/e2e/module.ts
@@ -23,6 +23,7 @@ import { E2eFilterBarStates } from '../components/filter-bar/e2e';
 import { E2eIconStateAndStyle } from '../components/icon/e2e';
 import { E2eListStates } from '../components/list/e2e';
 import { E2eMarkdownStates } from '../components/markdown/e2e';
+import { E2eProgressBarStateAndStyle } from '../components/progress-bar/e2e';
 import { E2eSearchExpandableStates } from '../components/search-expandable/e2e';
 import { E2eSplitButtonStateAndStyle } from '../components/split-button/e2e';
 import { E2eTagEditable, E2eTagStateAndStyle } from '../components/tags/e2e';
@@ -62,7 +63,8 @@ import { E2eToggleStateAndStyle, E2eToggleWithTextAndCaption } from '../componen
         E2eCheckboxStateAndStyle,
         E2eCheckboxWithTextAndCaption,
         E2eMarkdownStates,
-        E2eSearchExpandableStates
+        E2eSearchExpandableStates,
+        E2eProgressBarStateAndStyle
     ],
     templateUrl: 'template.html',
     styleUrl: 'main.scss',
