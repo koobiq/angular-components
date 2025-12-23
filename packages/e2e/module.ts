28ae4f40c5292dd00f0cc2888cdfd840f0a4eea9
lskramarov <lskramarov@users.noreply.github.com>
2025-12-23 17:31:40 +0300

    chore(navbar): added e2e tests (#DS-4402) (#1226)
    Co-authored-by: lskramarov <9027254+lskramarov@users.noreply.github.com>

--- packages/e2e/module.ts
+++ packages/e2e/module.ts
@@ -26,6 +26,7 @@ import { E2eIconStateAndStyle } from '../components/icon/e2e';
 import { E2eInputStateAndStyle } from '../components/input/e2e';
 import { E2eListStates } from '../components/list/e2e';
 import { E2eMarkdownStates } from '../components/markdown/e2e';
+import { E2eHorizontalNavbarStates, E2eVerticalNavbarStates } from '../components/navbar/e2e';
 import { E2eProgressBarStateAndStyle } from '../components/progress-bar/e2e';
 import { E2eSearchExpandableStates } from '../components/search-expandable/e2e';
 import { E2eSplitButtonStateAndStyle } from '../components/split-button/e2e';
@@ -70,7 +71,9 @@ import { E2eToggleStateAndStyle, E2eToggleWithTextAndCaption } from '../componen
         E2eMarkdownStates,
         E2eSearchExpandableStates,
         E2eProgressBarStateAndStyle,
-        E2eInputStateAndStyle
+        E2eInputStateAndStyle,
+        E2eHorizontalNavbarStates,
+        E2eVerticalNavbarStates
     ],
     templateUrl: 'template.html',
     styleUrl: 'main.scss',
