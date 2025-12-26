ba182e5c3d0d3f1ad7ab8845a63b0453029c12cf
lskramarov <l.s.kramarov@gmail.com>
2025-12-26 13:27:12 +0300

    fix: tests

--- packages/components/select/e2e.ts
+++ packages/components/select/e2e.ts
@@ -11,21 +11,33 @@ import { KbqSelectModule } from './select.module';
         FormsModule
     ],
     template: `
-        <div class="layout-row" style="width: 300px; height: 350px; gap: 16px" data-testid="e2eSelectStates">
-            <kbq-form-field>
-                <kbq-select data-testid="e2eSelect" [value]="'selected'">
-                    <kbq-optgroup class="cdk-keyboard-focused" [label]="'group'">
-                        <kbq-option [value]="'default'">default</kbq-option>
-                        <kbq-option class="kbq-hovered" [value]="'hovered'">hover</kbq-option>
-                        <kbq-option class="kbq-active" [value]="'active'">active</kbq-option>
-                        <kbq-option [value]="'selected'">selected</kbq-option>
-                        <kbq-option class="kbq-focused" [value]="'focused'">focused</kbq-option>
-                        <kbq-option [disabled]="true" [value]="'disabled'">disabled</kbq-option>
-                    </kbq-optgroup>
-                </kbq-select>
-            </kbq-form-field>
-        </div>
+        <kbq-form-field>
+            <kbq-select data-testid="e2eSelect" [value]="'selected'">
+                <kbq-optgroup class="cdk-keyboard-focused" [label]="'group'">
+                    <kbq-option [value]="'default'">default</kbq-option>
+                    <kbq-option class="kbq-hovered" [value]="'hovered'">hover</kbq-option>
+                    <kbq-option class="kbq-active" [value]="'active'">active</kbq-option>
+                    <kbq-option [value]="'selected'">selected</kbq-option>
+                    <kbq-option class="kbq-focused" [value]="'focused'">focused</kbq-option>
+                    <kbq-option [disabled]="true" [value]="'disabled'">disabled</kbq-option>
+                </kbq-optgroup>
+            </kbq-select>
+        </kbq-form-field>
     `,
+    styles: `
+        :host {
+            display: flex;
+            flex-direction: row;
+
+            width: 350px;
+            height: 350px;
+            gap: 16px;
+            padding: 8px;
+        }
+    `,
+    host: {
+        'data-testid': 'e2eSelectStates'
+    },
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class E2eSelectStates {}
@@ -37,26 +49,34 @@ export class E2eSelectStates {}
         FormsModule
     ],
     template: `
-        <div class="layout-row" style="width: 300px; height: 350px; gap: 16px" data-testid="e2eMultiselectStates">
-            <kbq-form-field style="width: 200px">
-                <kbq-select
-                    data-testid="e2eMultiSelect"
-                    [multiple]="true"
-                    [value]="['selected', 'default', 'default1']"
-                >
-                    <kbq-optgroup class="cdk-keyboard-focused" [label]="'group'">
-                        <kbq-option [value]="'default'">default</kbq-option>
-                        <kbq-option [value]="'default1'">default1 long name long name long name</kbq-option>
-                        <kbq-option class="kbq-hovered" [value]="'hovered'">hover</kbq-option>
-                        <kbq-option class="kbq-active" [value]="'active'">active</kbq-option>
-                        <kbq-option [value]="'selected'">selected</kbq-option>
-                        <kbq-option class="kbq-focused" [value]="'focused'">focused</kbq-option>
-                        <kbq-option [disabled]="true" [value]="'disabled'">disabled</kbq-option>
-                    </kbq-optgroup>
-                </kbq-select>
-            </kbq-form-field>
-        </div>
+        <kbq-form-field style="width: 200px">
+            <kbq-select data-testid="e2eMultiSelect" [multiple]="true" [value]="['selected', 'default', 'default1']">
+                <kbq-optgroup class="cdk-keyboard-focused" [label]="'group'">
+                    <kbq-option [value]="'default'">default</kbq-option>
+                    <kbq-option [value]="'default1'">default1 long name long name long name</kbq-option>
+                    <kbq-option class="kbq-hovered" [value]="'hovered'">hover</kbq-option>
+                    <kbq-option class="kbq-active" [value]="'active'">active</kbq-option>
+                    <kbq-option [value]="'selected'">selected</kbq-option>
+                    <kbq-option class="kbq-focused" [value]="'focused'">focused</kbq-option>
+                    <kbq-option [disabled]="true" [value]="'disabled'">disabled</kbq-option>
+                </kbq-optgroup>
+            </kbq-select>
+        </kbq-form-field>
     `,
+    styles: `
+        :host {
+            display: flex;
+            flex-direction: row;
+
+            width: 350px;
+            height: 350px;
+            gap: 16px;
+            padding: 8px;
+        }
+    `,
+    host: {
+        'data-testid': 'e2eMultiselectStates'
+    },
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class E2eMultiSelectStates {}
@@ -68,27 +88,39 @@ export class E2eMultiSelectStates {}
         FormsModule
     ],
     template: `
-        <div class="layout-row" style="width: 300px; height: 350px; gap: 16px" data-testid="e2eMultilineSelectStates">
-            <kbq-form-field style="width: 200px">
-                <kbq-select
-                    data-testid="e2eMultilineSelect"
-                    [multiple]="true"
-                    [multiline]="true"
-                    [value]="['selected', 'default', 'default1']"
-                >
-                    <kbq-optgroup class="cdk-keyboard-focused" [label]="'group'">
-                        <kbq-option [value]="'default'">default</kbq-option>
-                        <kbq-option [value]="'default1'">default1 long name long name long name</kbq-option>
-                        <kbq-option class="kbq-hovered" [value]="'hovered'">hover</kbq-option>
-                        <kbq-option class="kbq-active" [value]="'active'">active</kbq-option>
-                        <kbq-option [value]="'selected'">selected</kbq-option>
-                        <kbq-option class="kbq-focused" [value]="'focused'">focused</kbq-option>
-                        <kbq-option [disabled]="true" [value]="'disabled'">disabled</kbq-option>
-                    </kbq-optgroup>
-                </kbq-select>
-            </kbq-form-field>
-        </div>
+        <kbq-form-field style="width: 200px">
+            <kbq-select
+                data-testid="e2eMultilineSelect"
+                [multiple]="true"
+                [multiline]="true"
+                [value]="['selected', 'default', 'default1']"
+            >
+                <kbq-optgroup class="cdk-keyboard-focused" [label]="'group'">
+                    <kbq-option [value]="'default'">default</kbq-option>
+                    <kbq-option [value]="'default1'">default1 long name long name long name</kbq-option>
+                    <kbq-option class="kbq-hovered" [value]="'hovered'">hover</kbq-option>
+                    <kbq-option class="kbq-active" [value]="'active'">active</kbq-option>
+                    <kbq-option [value]="'selected'">selected</kbq-option>
+                    <kbq-option class="kbq-focused" [value]="'focused'">focused</kbq-option>
+                    <kbq-option [disabled]="true" [value]="'disabled'">disabled</kbq-option>
+                </kbq-optgroup>
+            </kbq-select>
+        </kbq-form-field>
     `,
+    styles: `
+        :host {
+            display: flex;
+            flex-direction: row;
+
+            width: 350px;
+            height: 360px;
+            gap: 16px;
+            padding: 8px;
+        }
+    `,
+    host: {
+        'data-testid': 'e2eMultilineSelectStates'
+    },
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class E2eMultilineSelectStates {}
@@ -102,36 +134,44 @@ export class E2eMultilineSelectStates {}
         KbqIconModule
     ],
     template: `
-        <div
-            class="layout-row"
-            style="width: 300px; height: 350px; gap: 16px"
-            data-testid="e2eSelectWithSearchAndFooter"
-        >
-            <kbq-form-field style="width: 200px">
-                <kbq-select data-testid="e2eSelect" [value]="'selected'">
-                    <kbq-form-field kbqFormFieldWithoutBorders kbqSelectSearch>
-                        <i kbq-icon="kbq-magnifying-glass_16" kbqPrefix></i>
-                        <input kbqInput type="text" [ngModel]="''" />
-                        <kbq-cleaner />
-                    </kbq-form-field>
+        <kbq-form-field style="width: 200px">
+            <kbq-select data-testid="e2eSelect" [value]="'selected'">
+                <kbq-form-field kbqFormFieldWithoutBorders kbqSelectSearch>
+                    <i kbq-icon="kbq-magnifying-glass_16" kbqPrefix></i>
+                    <input kbqInput type="text" [ngModel]="''" />
+                    <kbq-cleaner />
+                </kbq-form-field>
 
-                    <div kbq-select-search-empty-result>Nothing found</div>
+                <div kbq-select-search-empty-result>Nothing found</div>
 
-                    <kbq-optgroup class="cdk-keyboard-focused" [label]="'group'">
-                        <kbq-option [value]="'default'">default</kbq-option>
-                        <kbq-option [value]="'default1'">default1 long name long name long name</kbq-option>
-                        <kbq-option class="kbq-hovered" [value]="'hovered'">hover</kbq-option>
-                        <kbq-option class="kbq-active" [value]="'active'">active</kbq-option>
-                        <kbq-option [value]="'selected'">selected</kbq-option>
-                        <kbq-option class="kbq-focused" [value]="'focused'">focused</kbq-option>
-                        <kbq-option [disabled]="true" [value]="'disabled'">disabled</kbq-option>
-                    </kbq-optgroup>
+                <kbq-optgroup class="cdk-keyboard-focused" [label]="'group'">
+                    <kbq-option [value]="'default'">default</kbq-option>
+                    <kbq-option [value]="'default1'">default1 long name long name long name</kbq-option>
+                    <kbq-option class="kbq-hovered" [value]="'hovered'">hover</kbq-option>
+                    <kbq-option class="kbq-active" [value]="'active'">active</kbq-option>
+                    <kbq-option [value]="'selected'">selected</kbq-option>
+                    <kbq-option class="kbq-focused" [value]="'focused'">focused</kbq-option>
+                    <kbq-option [disabled]="true" [value]="'disabled'">disabled</kbq-option>
+                </kbq-optgroup>
 
-                    <kbq-select-footer>Caption ⌥+⌘+F</kbq-select-footer>
-                </kbq-select>
-            </kbq-form-field>
-        </div>
+                <kbq-select-footer>Caption ⌥+⌘+F</kbq-select-footer>
+            </kbq-select>
+        </kbq-form-field>
     `,
+    styles: `
+        :host {
+            display: flex;
+            flex-direction: row;
+
+            width: 350px;
+            height: 390px;
+            gap: 16px;
+            padding: 8px;
+        }
+    `,
+    host: {
+        'data-testid': 'e2eSelectWithSearchAndFooter'
+    },
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class E2eSelectWithSearchAndFooter {}
