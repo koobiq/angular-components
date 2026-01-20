e61f19d826dc9d1cc0f89133b9dfca0f6722aa1d
Artem Belik <artyombelyk@gmail.com>
2026-01-20 13:16:57 +0300

    refactor: tabs

--- packages/components/breadcrumbs/e2e.playwright-spec.ts
+++ packages/components/breadcrumbs/e2e.playwright-spec.ts
@@ -1,31 +1,35 @@
 import { expect, Locator, Page, test } from '@playwright/test';
-import { e2eEnableDarkTheme } from '../../e2e/utils';
+import { e2eEnableDarkTheme, e2eWhenStable } from 'packages/e2e/utils';
 
+// @TODO: should be fixed (#DS-4622)
 test.fixme('KbqBreadcrumbsModule', () => {
     test.describe('E2eBreadcrumbsStateAndStyle', () => {
         const getComponent = (page: Page) => page.getByTestId('e2eBreadcrumbsStateAndStyle');
-        const getTestTable = (locator: Locator) => locator.getByTestId('e2eBreadcrumbsTable');
-        const getTestRow = (locator: Locator) => locator.getByTestId('e2eBreadcrumbsDropdownRow');
-        const hoverLastItem = (locator: Locator) => locator.locator('.kbq-breadcrumb-item').last().hover();
+        const getBreadcrumbsWithDropdown = (locator: Locator) => locator.getByTestId('e2eBreadcrumbsWithDropdown');
+        const getLastBreadcrumbItem = (locator: Locator) => locator.locator('.kbq-breadcrumb-item').last();
 
         test('states', async ({ page }) => {
             await page.goto('/E2eBreadcrumbsStateAndStyle');
 
-            const locator = getComponent(page);
-            const screenshotTarget = getTestTable(locator);
+            const component = getComponent(page);
 
-            await expect(screenshotTarget).toHaveScreenshot('01-light.png');
+            await e2eWhenStable(component);
+
+            await expect(component).toHaveScreenshot('01-light.png');
             await e2eEnableDarkTheme(page);
-            await expect(screenshotTarget).toHaveScreenshot('01-dark.png');
+            await expect(component).toHaveScreenshot('01-dark.png');
         });
 
         test('dropdown and hover', async ({ page }) => {
             await page.goto('/E2eBreadcrumbsStateAndStyle');
 
-            const locator = getComponent(page);
-            const screenshotTarget = getTestRow(locator);
+            const component = getComponent(page);
 
-            await hoverLastItem(screenshotTarget);
+            await e2eWhenStable(component);
+
+            const screenshotTarget = getBreadcrumbsWithDropdown(component);
+
+            await getLastBreadcrumbItem(screenshotTarget).hover();
 
             await expect(screenshotTarget).toHaveScreenshot('02-light.png');
         });
--- packages/components/tabs/e2e.playwright-spec.ts
+++ packages/components/tabs/e2e.playwright-spec.ts
@@ -1,15 +1,20 @@
 import { expect, Page, test } from '@playwright/test';
-import { e2eEnableDarkTheme } from 'packages/e2e/utils';
+import { e2eEnableDarkTheme, e2eWhenStable } from 'packages/e2e/utils';
 
-test.fixme('KbqTabsModule', () => {
+test.describe('KbqTabsModule', () => {
     test.describe('E2eTabsStates', () => {
         const getComponent = (page: Page) => page.getByTestId('e2eTabsStates');
 
         test('states', async ({ page }) => {
             await page.goto('/E2eTabsStates');
-            await expect(getComponent(page)).toHaveScreenshot('01-light.png');
+
+            const component = getComponent(page);
+
+            await e2eWhenStable(component);
+
+            await expect(component).toHaveScreenshot('01-light.png');
             await e2eEnableDarkTheme(page);
-            await expect(getComponent(page)).toHaveScreenshot('01-dark.png');
+            await expect(component).toHaveScreenshot('01-dark.png');
         });
     });
 });
