40dd0e0e7f4bbdea196927095e1ee0db2c0bc0bc
Nikita Guryev <nikgurv@gmail.com>
2026-01-26 19:32:56 +0300

    chore(file-upload): updated examples

--- packages/docs-examples/components/file-upload/file-upload-single-with-size/file-upload-single-with-size-example.ts
+++ packages/docs-examples/components/file-upload/file-upload-single-with-size/file-upload-single-with-size-example.ts
@@ -1,6 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { KbqSingleFileUploadComponent } from '@koobiq/components/file-upload';
 import { KbqIconModule } from '@koobiq/components/icon';
+
+const createBlobPart = (sizeInMB: number): BlobPart => {
+    const chunk = 'a'.repeat(1024 * 1024);
+
+    return Array(sizeInMB).fill(chunk).join('');
+};
+
+const createMockFile = (fileName: string = 'Filename.txt', options?: FilePropertyBag) =>
+    new File([createBlobPart(2)] satisfies BlobPart[], fileName, options);
 
 /**
  * @title File-upload single with size
@@ -9,13 +19,16 @@ import { KbqIconModule } from '@koobiq/components/icon';
     selector: 'file-upload-single-with-size-example',
     imports: [
         KbqIconModule,
-        KbqSingleFileUploadComponent
+        KbqSingleFileUploadComponent,
+        ReactiveFormsModule
     ],
     template: `
-        <kbq-single-file-upload [showFileSize]="true">
-            <i kbq-icon="kbq-file-o_16"></i>
+        <kbq-single-file-upload [showFileSize]="true" [formControl]="control">
+            <i kbq-icon="kbq-file-text-o_16"></i>
         </kbq-single-file-upload>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FileUploadSingleWithSizeExample {}
+export class FileUploadSingleWithSizeExample {
+    control = new FormControl(createMockFile('Filename.txt', { type: 'text/plain' }));
+}
