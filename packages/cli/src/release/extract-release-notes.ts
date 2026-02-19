2f666d60f98de83e0fb9fc0600b2ce62b5a13af6
lskramarov <lskramarov@users.noreply.github.com>
2026-02-19 19:22:54 +0300

    chore(cli): error generating changelog for MM (#DS-4724) (#1375)

--- packages/cli/src/release/extract-release-notes.ts
+++ packages/cli/src/release/extract-release-notes.ts
@@ -5,18 +5,17 @@ export function extractReleaseNotes(changelogPath: string, versionName: string):
     const changelogContent = readFileSync(changelogPath, 'utf8');
     const escapedVersion = versionName.replace(new RegExp('\\.', 'g'), '\\.');
 
-    // Regular expression that matches the release notes for the given version. Note that we specify
-    // the "s" RegExp flag so that the line breaks will be ignored within our regex. We determine the
-    // section of a version by starting with the release header which can either use the markdown
-    // "h1" or "h2" syntax. The end of the section will be matched by just looking for the first
-    // subsequent release header.
-    const releaseNotesRegex = new RegExp(`##? (${escapedVersion} "?.*?"?.\\(.*?\\))(.*?)##? \\d+\\.\\d+`, 's');
+    // Regular expression that matches the release notes for the given version.
+    const releaseNotesRegex = new RegExp(
+        `##? (${escapedVersion} \\(\\d{4}-\\d{2}-\\d{2}\\)\n\n### .+?)\n\n([\\s\\S]*?)(?=\n# )`
+    );
+
     const matches = releaseNotesRegex.exec(changelogContent);
 
     return matches
         ? {
               releaseTitle: matches[1],
-              releaseNotes: matches[2].trim()
+              releaseNotes: matches[2]
           }
         : null;
 }
