b6dbf46547c9571e04fc0ca13704ecee0f478234
Nikita Guryev <nikgurv@gmail.com>
2026-03-12 19:40:04 +0300

    chore: fixed incorrect message text formatting for MM (#DS-4776)

--- packages/cli/src/release/extract-release-notes.ts
+++ packages/cli/src/release/extract-release-notes.ts
@@ -1,21 +1,31 @@
 import { readFileSync } from 'fs';
+import { marked, Token, Tokens } from 'marked';
+
+const isTagHeading = (item: Token): item is Tokens.Heading => item.type === 'heading' && item.depth <= 2;
 
 /** Extracts the release notes for a specific release from a given changelog file. */
 export function extractReleaseNotes(changelogPath: string, versionName: string): any {
     const changelogContent = readFileSync(changelogPath, 'utf8');
-    const escapedVersion = versionName.replace(new RegExp('\\.', 'g'), '\\.');
 
-    // Regular expression that matches the release notes for the given version.
-    const releaseNotesRegex = new RegExp(
-        `##? (${escapedVersion} \\(\\d{4}-\\d{2}-\\d{2}\\)\n\n### .+?)\n\n([\\s\\S]*?)(?=\n##? )`
+    const parsedChangelogItems = marked.Lexer.lex(changelogContent);
+
+    const currentVersionIndex = parsedChangelogItems.findIndex(
+        (item: Token) => isTagHeading(item) && item.text.includes(versionName)
     );
+    const previousVersionIndex = parsedChangelogItems.slice(currentVersionIndex + 1).findIndex(isTagHeading);
 
-    const matches = releaseNotesRegex.exec(changelogContent);
+    if (!currentVersionIndex && !previousVersionIndex) return null;
 
-    return matches
-        ? {
-              releaseTitle: matches[1],
-              releaseNotes: matches[2]
-          }
-        : null;
+    // merge all data between two versions
+    const releaseNotes = parsedChangelogItems
+        .slice(currentVersionIndex + 1, previousVersionIndex)
+        .map((item) => item.raw)
+        .join('');
+
+    const releaseTitle = (parsedChangelogItems[currentVersionIndex] as Tokens.Heading).text;
+
+    return {
+        releaseNotes,
+        releaseTitle
+    };
 }
