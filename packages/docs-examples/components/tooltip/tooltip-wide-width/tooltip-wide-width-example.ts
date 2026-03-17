abe2d9d2c327397e256df2ec80ad817a9851f465
lskramarov <l.s.kramarov@gmail.com>
2026-03-17 09:49:09 +0300

    fix: md and example with wide tooltip

--- packages/docs-examples/components/tooltip/tooltip-wide-width/tooltip-wide-width-example.ts
+++ packages/docs-examples/components/tooltip/tooltip-wide-width/tooltip-wide-width-example.ts
@@ -9,8 +9,18 @@ import { KbqToolTipModule } from '@koobiq/components/tooltip';
     imports: [KbqToolTipModule],
     template: `
         <div class="layout-row layout-align-center-center" style="min-height: 120px; gap: var(--kbq-size-l);">
-            <span kbqTooltip="9f1c2e4a8b7d3f6c5e2a1d9b0c4e7f8a6d3c2b1a9e8f7d6c5b4a3e2d1c0f9a8">9f1c...f9a8</span>
+            <span
+                kbqTooltip="9f1c2e4a8b7d3f6c5e2a1d9b0c4e7f8a6d3c2b1a9e8f7d6c5b4a3e2d1c0f9a8"
+                [kbqTooltipClass]="'example__wide-tooltip'"
+            >
+                9f1c...f9a8
+            </span>
         </div>
+    `,
+    styles: `
+        ::ng-deep .example__wide-tooltip.kbq-tooltip {
+            --kbq-tooltip-size-max-width: 454px;
+        }
     `,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
