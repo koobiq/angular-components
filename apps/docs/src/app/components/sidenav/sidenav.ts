7901168375bf29112a44b8f59968551f7941ffb0
lskramarov <l.s.kramarov@gmail.com>
2026-05-29 17:24:26 +0300

    fix: docs

--- apps/docs/src/app/components/sidenav/sidenav.ts
+++ apps/docs/src/app/components/sidenav/sidenav.ts
@@ -133,7 +133,7 @@ export class DocsSidenav extends DocsLocaleState implements AfterViewInit {
 
     ngAfterViewInit() {
         this.treeControl.expandAll();
-        this.docStates.registerNavbarScrollContainer(this.scrollbar().contentElement.nativeElement);
+        this.docStates.registerNavbarScrollContainer(this.scrollbar().contentElement().nativeElement);
         setTimeout(() => this.tree().highlightSelectedOption());
     }
 
