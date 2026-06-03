b54196ae6cc2505de197b92d2cf2a4c95778ed36
Artem Belik <artyombelyk@gmail.com>
2026-06-03 12:33:02 +0300

    refactor: example

--- packages/docs-examples/components/tabs/tabs-underlined/tabs-underlined-example.ts
+++ packages/docs-examples/components/tabs/tabs-underlined/tabs-underlined-example.ts
@@ -7,68 +7,37 @@ import { KbqTabsModule } from '@koobiq/components/tabs';
  */
 @Component({
     selector: 'tabs-underlined-example',
-    imports: [
-        KbqTabsModule,
-        KbqIconModule
-    ],
+    imports: [KbqTabsModule, KbqIconModule],
     template: `
         <kbq-tab-group class="layout-margin-bottom-l" underlined>
-            <kbq-tab label="BruteForce" />
-            <kbq-tab label="Complex Attack" />
-            <kbq-tab label="DDoS" />
-            <kbq-tab label="DoS" />
+            @for (tab of tabs; track tab) {
+                <kbq-tab [label]="tab" />
+            }
         </kbq-tab-group>
 
         <kbq-tab-group class="layout-margin-bottom-l" underlined>
-            <kbq-tab>
-                <ng-template kbq-tab-label>
-                    <i kbq-icon="kbq-bug_16"></i>
-                    Spam Attack
-                </ng-template>
-            </kbq-tab>
-            <kbq-tab>
-                <ng-template kbq-tab-label>
-                    <i kbq-icon="kbq-bug_16"></i>
-                    Miscellaneous
-                </ng-template>
-            </kbq-tab>
-            <kbq-tab>
-                <ng-template kbq-tab-label>
-                    <i kbq-icon="kbq-bug_16"></i>
-                    DDoS
-                </ng-template>
-            </kbq-tab>
-            <kbq-tab>
-                <ng-template kbq-tab-label>
-                    <i kbq-icon="kbq-bug_16"></i>
-                    IDS/IPS Alert
-                </ng-template>
-            </kbq-tab>
+            @for (tab of tabs; track tab) {
+                <kbq-tab>
+                    <ng-template kbqTabLabel>
+                        <i kbq-icon="kbq-bug_16"></i>
+                        {{ tab }}
+                    </ng-template>
+                </kbq-tab>
+            }
         </kbq-tab-group>
 
         <kbq-tab-group underlined>
-            <kbq-tab>
-                <ng-template kbq-tab-label>
-                    <i kbq-icon="kbq-bug_16"></i>
-                </ng-template>
-            </kbq-tab>
-            <kbq-tab>
-                <ng-template kbq-tab-label>
-                    <i kbq-icon="kbq-bug_16"></i>
-                </ng-template>
-            </kbq-tab>
-            <kbq-tab>
-                <ng-template kbq-tab-label>
-                    <i kbq-icon="kbq-bug_16"></i>
-                </ng-template>
-            </kbq-tab>
-            <kbq-tab>
-                <ng-template kbq-tab-label>
-                    <i kbq-icon="kbq-bug_16"></i>
-                </ng-template>
-            </kbq-tab>
+            @for (tab of tabs; track tab) {
+                <kbq-tab>
+                    <ng-template kbqTabLabel iconOnly>
+                        <i kbq-icon="kbq-bug_16"></i>
+                    </ng-template>
+                </kbq-tab>
+            }
         </kbq-tab-group>
     `,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TabsUnderlinedExample {}
+export class TabsUnderlinedExample {
+    readonly tabs = ['BruteForce', 'Complex Attack', 'DDoS', 'HIPS alert'];
+}
