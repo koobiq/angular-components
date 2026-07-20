112d013c9651a0fbbe39d644c839aece528def3f
Nikita Guryev <nikgurv@gmail.com>
2026-07-20 10:17:16 +0300

    chore: after review

--- packages/docs-examples/components/clamped-text/clamped-text-overview/clamped-text-overview-example.ts
+++ packages/docs-examples/components/clamped-text/clamped-text-overview/clamped-text-overview-example.ts
@@ -1,21 +1,18 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { KbqClampedText } from '@koobiq/components/clamped-text';
-import { KbqToggleModule } from '@koobiq/components/toggle';
 
 /**
  * @title Clamped-text overview
  */
 @Component({
     selector: 'clamped-text-overview-example',
-    imports: [KbqClampedText, KbqToggleModule],
+    imports: [KbqClampedText],
     template: `
         <div class="layout-margin-bottom-l">
-            <kbq-clamped-text [isCollapsed]="collapsed()" (isCollapsedChange)="collapsed.set($event)">
+            <kbq-clamped-text>
                 {{ text }}
             </kbq-clamped-text>
         </div>
-
-        <kbq-toggle [checked]="collapsed()" (change)="collapsed.set($event.checked)">Collapsed</kbq-toggle>
     `,
     styles: `
         :host > div {
@@ -29,7 +26,6 @@ import { KbqToggleModule } from '@koobiq/components/toggle';
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ClampedTextOverviewExample {
-    protected readonly collapsed = signal(true);
     protected readonly text =
         'In a distributed denial-of-service attack (DDoS attack), the incoming traffic flooding the victim originates from many different sources. More sophisticated strategies are required to mitigate this type of attack; simply attempting to block a single source is insufficient as there are multiple sources. A DoS or DDoS attack is analogous to a group of people crowding the entry door of a shop, making it hard for legitimate customers to enter, thus disrupting trade and losing the business money. Criminal perpetrators of DoS attacks often target sites or services hosted on high-profile web servers such as banks or credit card payment gateways. Revenge and blackmail, as well as hacktivism, can motivate these attacks.';
 }
