c6a04108bdabd7c272d11a2be74cc0ca5175a0c8
Artem Belik <artyombelyk@gmail.com>
2026-07-30 09:55:36 +0300

    chore(docs): added link to github sources (#DS-3565) (#1778)

--- apps/docs/src/app/version.ts
+++ apps/docs/src/app/version.ts
@@ -1,6 +1,9 @@
 import { Version } from '@angular/core';
 import { VERSION } from '@koobiq/components/core';
 
-const version = VERSION.full === '{{VERSION}}' ? new Version('0.0.0-dev') : VERSION;
+/** Placeholder version used when the `{{VERSION}}` build-time token hasn't been replaced, i.e. in local dev builds. */
+export const docsDevVersionPlaceholder = '0.0.0-dev';
+
+const version = VERSION.full === '{{VERSION}}' ? new Version(docsDevVersionPlaceholder) : VERSION;
 
 export const docsKoobiqVersion = version.full.match(/(\d+\.\d+\.\d+(?:[^\+]*))/)![1];
