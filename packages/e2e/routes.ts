0f895ea096d7537760ef4e3502c345665dcc055e
Artem Belik <artyombelyk@gmail.com>
2026-07-30 18:29:55 +0300

    refactor: e2e

--- packages/e2e/routes.ts
+++ packages/e2e/routes.ts
@@ -70,6 +70,7 @@ import { E2ePopoverPositioning, E2ePopoverStates } from '../components/popover/e
 import { E2eProgressBarStateAndStyle } from '../components/progress-bar/e2e';
 import { E2eProgressSpinnerStates } from '../components/progress-spinner/e2e';
 import { E2eRadioStateAndStyle } from '../components/radio/e2e';
+import { E2eScrollbarStateAndStyle } from '../components/scrollbar/e2e';
 import { E2eSearchExpandableStates } from '../components/search-expandable/e2e';
 import {
     E2eMultilineSelectStates,
@@ -257,7 +258,8 @@ const components = [
     E2eOverflowItemsHorizontal,
     E2eOverflowItemsVertical,
     E2eOverflowItemsOrdered,
-    E2eOverflowItemsDynamic
+    E2eOverflowItemsDynamic,
+    E2eScrollbarStateAndStyle
 ];
 
 export const e2eRoutes: Routes = components.map((component) => {
