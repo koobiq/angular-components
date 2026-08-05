e57db72e088bc717682772ad84a99bf6e3671ab1
Nikita Guryev <nikgurv@gmail.com>
2026-08-05 10:58:28 +0300

    chore: linters

--- packages/components/form-field/form-field.ts
+++ packages/components/form-field/form-field.ts
@@ -27,9 +27,7 @@ import {
 } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { NgControl } from '@angular/forms';
-import {     KBQ_CONNECTED_OVERLAY_ORIGIN,
-    KBQ_FORM_FIELD_REF,
-    KbqColorDirective } from '@koobiq/components/core';
+import { KBQ_CONNECTED_OVERLAY_ORIGIN, KBQ_FORM_FIELD_REF, KbqColorDirective } from '@koobiq/components/core';
 import { EMPTY, merge } from 'rxjs';
 import { delay, startWith } from 'rxjs/operators';
 import { KbqCleaner } from './cleaner';
