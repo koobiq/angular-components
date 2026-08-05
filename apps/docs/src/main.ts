062d15f0564bea6bc7fa91eda4739a9fb84716ae
Artem Belik <artyombelyk@gmail.com>
2026-08-05 17:53:47 +0300

    feat(docs): added Sentry integration (#DS-3688)

--- apps/docs/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { DocsAppComponent } from './app/app.component';
-import { appConfig } from './app/config';
-
-bootstrapApplication(DocsAppComponent, appConfig).catch((err) => console.error(err));
