1c121573b5a35ca26c57b6b73f9dca90363648f7
Artem Belik <artyombelyk@gmail.com>
2026-08-27 18:28:33 +0300

    refactor: review

--- packages/components-dev/ssr/config.server.ts
+++ packages/components-dev/ssr/config.server.ts
@@ -10,9 +10,7 @@ import { devTimezoneServerProvider } from './timezone';
 class SsrErrorHandler implements ErrorHandler {
     handleError(error: unknown): never {
         // Preserve default logging behavior, but fail the build.
-        // eslint-disable-next-line no-console
         console.error(error);
-
         throw error instanceof Error ? error : new Error(String(error));
     }
 }
