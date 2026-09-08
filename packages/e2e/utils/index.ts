ccf670b95f9bbec3b9f3621007bc6fba4f1c670b
Nikita Guryev <nikgurv@gmail.com>
2026-09-08 16:01:16 +0300

    chore(tabs): fixed flacky tests

--- packages/e2e/utils/index.ts
+++ packages/e2e/utils/index.ts
@@ -1,5 +1,6 @@
 export * from './autofill';
 export * from './focus-scroll';
 export * from './overflow-shadow';
+export * from './scroll-position';
 export * from './scrollbar';
 export * from './theme';
